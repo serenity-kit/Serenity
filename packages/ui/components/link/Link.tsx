dbfdada8de4d7d2a732b572365856fda7d6d9506
Nik Graf <nik@nikgraf.com>
2022-03-22 11:01:52 +0100

    fix type definitions

--- packages/ui/components/link/Link.tsx
+++ packages/ui/components/link/Link.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import * as React from "react";
 import { tw } from "../../tailwind";
 import { Link as ReactNavigationLink } from "@react-navigation/native";
+import type { NavigationAction } from "@react-navigation/core";
+import { GestureResponderEvent, Text, TextProps } from "react-native";
+import { To } from "@react-navigation/native/lib/typescript/src/useLinkTo";
 
-export function Link(props) {
+// copied from react-navigation type definitions
+declare type Props<ParamList extends ReactNavigation.RootParamList> = {
+  to: To<ParamList>;
+  action?: NavigationAction;
+  target?: string;
+  onPress?: (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent> | GestureResponderEvent
+  ) => void;
+} & (TextProps & {
+  children: React.ReactNode;
+});
+
+export function Link<ParamList extends ReactNavigation.RootParamList>(
+  props: Props<ParamList>
+) {
   return (
     <ReactNavigationLink
       {...props}
+      // @ts-expect-error allow style overwrite
       style={tw.style(`text-blue-500 dark:text-blue-500`, props.style)}
     />
   );
