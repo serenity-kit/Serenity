02c78691b3620caf6988a0107ce03cb618213f5b
Nik Graf <nik@nikgraf.com>
2022-04-07 19:35:31 +0200

    Improve formatting setup (#27)
    * upgrade prettier
    
    * add command to check formatting of all files
    
    * add prettier check to ci

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,27 @@
 module.exports = {
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: '#FFFFFF',
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#FFFFFF",
       gray: {
-        100: '#F8F8F8',
-        200: '#E4E5ED',
-        400: '#CBCBD3',
-        600: '#8A8B96',
-        800: '#4F5057',
-        900: '#1F1F1F',
+        100: "#F8F8F8",
+        200: "#E4E5ED",
+        400: "#CBCBD3",
+        600: "#8A8B96",
+        800: "#4F5057",
+        900: "#1F1F1F",
       },
       primary: {
-        400: '#91A0FF',
-        500: '#435BF8',
-        600: '#180177',
+        400: "#91A0FF",
+        500: "#435BF8",
+        600: "#180177",
       },
-      emerald: '#48AD83',
-      amber: '#F09334',
-      red: '#EC3153',
+      emerald: "#48AD83",
+      amber: "#F09334",
+      red: "#EC3153",
     },
     extend: {},
   },
   plugins: [],
-}
+};
