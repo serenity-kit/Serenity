46d14ab10d4f1e49d3e1757f01304ce80db4d659
Adonis Gaitatzis <adonis@Adoniss-MacBook-Pro-2.local>
2022-04-14 13:00:44 +0200

    adding libsodium base64-compatible encoding/decoding and tests

--- packages/naisho-core/src/crypto.test.ts
+++ packages/naisho-core/src/crypto.test.ts
@@ -3,35 +3,7 @@ import serenitySodium from "@serenity-tools/libsodium";
 import sodium from "libsodium-wrappers-sumo";
 // import { v4 as uuidv4 } from "uuid";
 // import { encryptAead, decryptAead } from "./crypto";
-declare const Buffer;
-
-/*
-test("encryptAead and decryptAead", async () => {
-  await sodium.ready;
-
-  const key = sodiumWrapper.from_hex(
-    "724b092810ec86d7e35c9d067702b31ef90bc43a7b598626749914d6a3e033ed"
-  );
-
-  const publicData = {
-    snapshotId: uuidv4(),
-  };
-
-  const encryptedResult = await encryptAead(
-    "Hallo",
-    sodiumWrapper.to_base64(JSON.stringify(publicData)),
-    sodium.to_base64(key)
-  );
-
-  const decryptedResult = await decryptAead(
-    sodium.from_base64(encryptedResult.ciphertext),
-    sodiumWrapper.to_base64(JSON.stringify(publicData)),
-    sodium.to_base64(key),
-    encryptedResult.publicNonce
-  );
-  expect(sodium.from_base64_to_string(decryptedResult)).toEqual("Hallo");
-});
-/* */
+declare const Buffer: any;
 
 test("Can encode libsodium-compatible base64", async () => {
   await sodium.ready;
