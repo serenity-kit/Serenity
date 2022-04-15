11615d8d5992ed15d0cdda88528e45686c148fed
Nik Graf <nik@nikgraf.com>
2022-04-15 12:57:18 +0200

    setup button

--- apps/app/screens/DesignSystemScreen.tsx
+++ apps/app/screens/DesignSystemScreen.tsx
@@ -2,7 +2,7 @@ import { Text, tw, View, Button } from "@serenity-tools/ui";
 
 export default function DesignSystemScreen() {
   return (
-    <View style={tw`mt-20`}>
+    <View style={tw`mt-20 px-4`}>
       <Text>Default Button</Text>
       <Button>Login</Button>
       <Text>Disabled Button</Text>
--- packages/ui/components/button/Button.tsx
+++ packages/ui/components/button/Button.tsx
@@ -1,32 +1,75 @@
-import React from "react";
-import { Button as NativeBaseButton } from "native-base";
+// inspired by https://github.com/GeekyAnts/NativeBase/blob/master/src/components/primitives/Button/Button.tsx
+
+import React, { forwardRef } from "react";
+import { Text as RnText, View as RnView } from "react-native";
+import { useFocusRing } from "@react-native-aria/focus";
+import { Pressable } from "native-base";
 import { tw } from "../../tailwind";
 
 const baseStyle = tw`rounded px-4 py-4`;
 
-export function Button(props) {
+const computeStyle = (
+  disabled: boolean,
+  isPressed: boolean,
+  isHovered: boolean,
+  isFocusVisible: boolean,
+  isFocused: boolean
+) => {
+  if (disabled) return tw`bg-gray-400`;
+
+  let style: any = isFocusVisible
+    ? {
+        boxShadow: `${tw.color("primary-900")} 0px 0px 0px 4px`,
+      }
+    : {};
+  if (isPressed) return tw.style(`bg-primary-200`);
+  if (isHovered) return tw.style(`bg-primary-300`, style);
+  return style;
+};
+
+type Props = {
+  children: React.ReactNode;
+  disabled?: boolean;
+};
+
+export const Button = (props) => {
+  const { isFocusVisible, focusProps: focusRingProps } = useFocusRing();
+
   return (
-    <NativeBaseButton
+    <Pressable
       {...props}
-      style={tw.style(baseStyle, `bg-primary-500`, props.style)}
-      _text={{
-        style: tw`text-base text-center text-gray-100`,
+      rounded
+      accessibilityRole={props.accessibilityRole ?? "button"}
+      // @ts-expect-error - web only
+      onFocus={focusRingProps.onFocus}
+      // @ts-expect-error - web only
+      onBlur={focusRingProps.onBlur}
+      // disable default outline styles
+      _focusVisible={{ _web: { style: { outlineWidth: 0 } } }}
+    >
+      {({ isPressed, isHovered, isFocused }) => {
+        return (
+          <RnView
+            style={tw.style(
+              baseStyle,
+              `bg-primary-500`,
+              computeStyle(
+                props.disabled,
+                isPressed,
+                isHovered,
+                isFocusVisible,
+                isFocused
+              ),
+              { cursor: props.disabled ? "not-allowed" : "pointer" }, // web only
+              props.style
+            )}
+          >
+            <RnText style={tw`text-base text-center text-gray-100`}>
+              {props.children}
+            </RnText>
+          </RnView>
+        );
       }}
-      _hover={{
-        style: tw.style(baseStyle, `bg-primary-400`, props.hoverStyle),
-      }}
-      _pressed={{
-        style: tw.style(baseStyle, `bg-primary-600`, props.pressedStyle),
-      }}
-      _focus={{
-        style: tw.style(baseStyle, `bg-primary-500`, props.focusStyle),
-      }}
-      _focusVisible={{
-        style: tw.style(baseStyle, `bg-primary-500`, props.focusVisibleStyle),
-      }}
-      _disabled={{
-        style: tw.style(baseStyle, `bg-gray-500`, props.disabledStyle),
-      }}
-    />
+    </Pressable>
   );
-}
+};
