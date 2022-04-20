ba0d2361de7a6884f57ab601cedc137e9de43af9
Nik Graf <nik@nikgraf.com>
2022-04-20 13:37:24 +0200

    add italic icon

--- packages/ui/components/icon/Icon.tsx
+++ packages/ui/components/icon/Icon.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { tw } from "../../tailwind";
 import { Bold } from "./icons/Bold";
+import { Italic } from "./icons/Italic";
 import { ListCheck2 } from "./icons/ListCheck2";
 import { ListUnordered } from "./icons/ListUnordered";
 
 export type Props = {
-  name: "bold" | "list-check-2" | "list-unordered";
+  name: "bold" | "italic" | "list-check-2" | "list-unordered";
   color?: string;
   size?: number;
 };
@@ -15,6 +16,7 @@ export const Icon = (props: Props) => {
   const color = props.color ?? (tw.color("gray-900") as string);
   const size = props.size ?? 24;
   if (name === "bold") return <Bold color={color} size={size} />;
+  if (name === "italic") return <Italic color={color} size={size} />;
   if (name === "list-check-2") return <ListCheck2 color={color} size={size} />;
   if (name === "list-unordered")
     return <ListUnordered color={color} size={size} />;
