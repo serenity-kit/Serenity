215b995d4438c9e64c8d7f9ebab13d8226ba8f7f
skristufek <s.kristufek@invitario.com>
2022-04-27 14:52:07 +0200

    Use mobily/stacks to get order into design-system screen

--- apps/app/navigation/screens/DesignSystemScreen.tsx
+++ apps/app/navigation/screens/DesignSystemScreen.tsx
@@ -1,4 +1,6 @@
 import { Text, tw, View, Button, Input, Icon } from "@serenity-tools/ui";
+import { Columns, Column, Tiles } from "@mobily/stacks";
+import React from "react";
 
 export default function DesignSystemScreen() {
   return (
@@ -17,15 +19,37 @@ export default function DesignSystemScreen() {
       <Input disabled value="jane@example.com" />
       <Text>Input Disabled</Text>
       <Input disabled placeholder="Enter your email …" />
-      <Text>Icons</Text>
-      <Icon name="bold" />
-      <Icon name="italic" />
-      <Icon name="list-check-2" />
-      <Icon name="list-unordered" />
-      <Text>Icon resized</Text>
-      <Icon name="list-unordered" size={16} />
-      <Icon name="list-unordered" size={32} />
-      <Text>Icons coloured</Text>
+      <Text style={tw`mt-6 mb-4 font-700 text-xl text-center`}>Icons</Text>
+      <Text style={tw`mb-1`}>Marks</Text>
+      <Tiles style={tw`max-w-lg`} space={4} columns={10}>
+        <Icon name="bold" />
+        <Icon name="italic" />
+      </Tiles>
+      <Text style={tw`mt-4 mb-1`}>Nodes</Text>
+      <Tiles style={tw`max-w-lg`} space={4} columns={10}>
+        <Icon name="list-check-2" />
+        <Icon name="list-ordered" />
+        <Icon name="list-unordered" />
+      </Tiles>
+      <Text style={tw`mt-4 mb-1`}>Extension</Text>
+      <Tiles style={tw`max-w-lg`} space={4} columns={10}></Tiles>
+      <Text style={tw`mt-4 mb-1`}>UI</Text>
+      <Tiles style={tw`max-w-lg`} space={4} columns={10}>
+        <Icon name="archive-line" />
+        <Icon name="at-line" />
+        <Icon name="calendar-check-fill" />
+        <Icon name="printer-line" />
+      </Tiles>
+      <Text style={tw`mt-4 mb-1`}>Icons resized</Text>
+      <Columns space={4} alignY="center" alignX="left">
+        <Column width="content">
+          <Icon name="list-unordered" size={16} />
+        </Column>
+        <Column width="content">
+          <Icon name="list-unordered" size={32} />
+        </Column>
+      </Columns>
+      <Text style={tw`mt-4 mb-1`}>Icons coloured</Text>
       <Icon name="list-check-2" color={tw.color("primary-500")} />
     </View>
   );
