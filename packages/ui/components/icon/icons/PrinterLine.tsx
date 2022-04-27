ce0daa72d35f62c2aa2d4d30ac2d5c93a8f5d709
skristufek <s.kristufek@invitario.com>
2022-04-27 14:50:02 +0200

    add Icons: archive-line, at-line, calendar-check-fill, list-ordered and printer-line

---
+++ packages/ui/components/icon/icons/PrinterLine.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+
+export type Props = { color: string; size: number };
+
+export const PrinterLine = ({ color, size }: Props) => {
+  return (
+    <Svg height={size} width={size} viewBox="0 0 24 24">
+      <Path fill="none" d="M0 0h24v24H0z" />
+      <Path
+        fill={color}
+        d="M6 19H3a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h3V3a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v4h3a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-3v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-2zm0-2v-1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v1h2V9H4v8h2zM8 4v3h8V4H8zm0 13v3h8v-3H8zm-3-7h3v2H5v-2z"
+      />
+    </Svg>
+  );
+};
