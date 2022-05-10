69980b88cb056797f5257eb88f8f8e05725b67cb
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-05-10 16:36:06 +0200

    adjust styling for checkbox states according to Design System

--- packages/ui/components/checkbox/Checkbox.tsx
+++ packages/ui/components/checkbox/Checkbox.tsx
@@ -7,7 +7,9 @@ import { Checkbox as NativeBaseCheckbox, ICheckboxProps } from "native-base";
 type CheckboxProps = ICheckboxProps & {};
 
 const styles = StyleSheet.create({
-  checkbox: tw`bg-white`,
+  default: tw`bg-white border-gray-800`,
+  // nb-override: opacity
+  disabled: tw`border-gray-300 opacity-100`,
 });
 
 export const Checkbox = React.forwardRef(
@@ -16,20 +18,31 @@ export const Checkbox = React.forwardRef(
       <NativeBaseCheckbox
         ref={ref}
         {...rest}
-        style={[styles.checkbox, rest.style]}
-        _hover={{
-          style: tw`bg-gray-300`,
-        }}
+        style={[styles.default, rest.style]}
+        borderWidth={1}
         _disabled={{
-          style: styles.checkbox,
+          style: [tw`bg-white`, styles.disabled],
+        }}
+        _hover={{
+          style: tw`bg-gray-300 border-gray-800`,
         }}
         _checked={{
-          style: tw`bg-primary-200`,
+          style: tw`bg-primary-400 border-primary-400`,
+          _disabled: {
+            style: [tw`bg-gray-300`, styles.disabled],
+          },
+          _hover: {
+            style: tw`bg-primary-500 border-primary-500`,
+            _disabled: {
+              style: [tw`bg-gray-300`, styles.disabled],
+            },
+          },
         }}
         // @ts-expect-error this prop is simply not exposed
         _focusVisible={{
           _interactionBox: {
-            style: tw`w-full h-full se-outline-focus`,
+            // nb-override: sizing definitions needed to override default of 130% so shadow-outline actually surrounds checkbox
+            style: tw`w-full h-full se-outline-focus-mini`,
           },
         }}
       >
