4b5769f71d8eb83c51b46aab6c9ee5d0b9f46222
Nik Graf <nik@nikgraf.com>
2022-05-12 17:55:21 +0200

    add basic styling

--- apps/app/components/pageHeaderRight/PageHeaderRight.tsx
+++ apps/app/components/pageHeaderRight/PageHeaderRight.tsx
@@ -1,5 +1,5 @@
 import { useHasEditorSidebar } from "@serenity-tools/editor/hooks/useHasEditorSidebar";
-import { Text } from "@serenity-tools/ui";
+import { Text, tw, View } from "@serenity-tools/ui";
 import React from "react";
 
 export function PageHeaderRight() {
@@ -9,5 +9,9 @@ export function PageHeaderRight() {
     return <Text>Mobile Header Right</Text>;
   }
 
-  return <Text>Desktop Header Right</Text>;
+  return (
+    <View style={tw`h-full w-60 border-l border-b border-gray-200 bg-gray-100`}>
+      <Text>Desktop Header Right</Text>
+    </View>
+  );
 }
