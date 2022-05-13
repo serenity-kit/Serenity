a4e9641a30f41aaa5cd5b54215385316ece10132
Nik Graf <nik@nikgraf.com>
2022-05-13 11:52:50 +0200

    style editor sidebar

--- packages/editor/components/editorSidebar/EditorSidebar.tsx
+++ packages/editor/components/editorSidebar/EditorSidebar.tsx
@@ -20,8 +20,61 @@ export default function EditorSidebar({
   headingLevels,
 }: EditorSidebarProps) {
   return (
-    <View style={tw`w-60 h-full border-l border-gray-200 bg-gray-100`}>
+    <View style={tw`w-60 h-full border-l border-gray-200 bg-gray-100 pt-4`}>
       <div>
+        <Text variant="tiny" style={tw`flex ml-4 mb-2`}>
+          Decorations
+        </Text>
+
+        <SidebarButton
+          onPress={() => editor?.chain().focus().toggleBold().run()}
+        >
+          <EditorSidebarIcon
+            isActive={editor?.isActive("bold") || false}
+            name="bold"
+          />
+          <Text variant="small">Bold</Text>
+        </SidebarButton>
+
+        <SidebarButton
+          onPress={() => editor?.chain().focus().toggleItalic().run()}
+        >
+          <EditorSidebarIcon
+            isActive={editor?.isActive("italic") || false}
+            name="italic"
+          />
+          <Text variant="small">Italic</Text>
+        </SidebarButton>
+
+        <SidebarButton
+          onPress={() => editor?.chain().focus().toggleCode().run()}
+        >
+          <EditorSidebarIcon
+            isActive={editor?.isActive("code") || false}
+            name="code-view"
+          />
+          <Text variant="small">Code</Text>
+        </SidebarButton>
+
+        {/* styling dummy */}
+        <SidebarButton
+          onPress={() =>
+            editor?.chain().focus().toggleLink({ href: "#" }).run()
+          }
+        >
+          <EditorSidebarIcon
+            isActive={editor?.isActive("link") || false}
+            name="link"
+          />
+          <Text variant="small">Link</Text>
+        </SidebarButton>
+
+        <SidebarDivider />
+
+        <Text variant="tiny" style={tw`flex ml-4 mb-2`}>
+          Blocks
+        </Text>
+
         {headingLevels.map((lvl) => {
           return (
             <SidebarButton
@@ -34,54 +87,10 @@ export default function EditorSidebar({
                 isActive={editor?.isActive("heading", { level: lvl }) || false}
                 name="heading"
               />
-              <Text>Headline {lvl}</Text>
+              <Text variant="small">Headline {lvl}</Text>
             </SidebarButton>
           );
         })}
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleBold().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("bold") || false}
-            name="bold"
-          />
-          <Text>Bold</Text>
-        </SidebarButton>
-
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleItalic().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("italic") || false}
-            name="italic"
-          />
-          <Text>Italic</Text>
-        </SidebarButton>
-
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleCode().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("code") || false}
-            name="code-view"
-          />
-          <Text>Code</Text>
-        </SidebarButton>
-
-        <SidebarDivider />
-
-        {/* styling dummy */}
-        <SidebarButton
-          onPress={() =>
-            editor?.chain().focus().toggleLink({ href: "#" }).run()
-          }
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("link") || false}
-            name="link"
-          />
-          <Text>Link</Text>
-        </SidebarButton>
 
         <SidebarButton
           onPress={() => editor?.chain().focus().toggleCodeBlock().run()}
@@ -90,7 +99,7 @@ export default function EditorSidebar({
             isActive={editor?.isActive("codeBlock") || false}
             name="code-s-slash-line"
           />
-          <Text>Codeblock</Text>
+          <Text variant="small">Codeblock</Text>
         </SidebarButton>
 
         <SidebarButton
@@ -100,10 +109,14 @@ export default function EditorSidebar({
             isActive={editor?.isActive("blockquote") || false}
             name="question-mark"
           />
-          <Text>Blockquote</Text>
+          <Text variant="small">Blockquote</Text>
         </SidebarButton>
 
         <SidebarDivider />
+
+        <Text variant="tiny" style={tw`flex ml-4 mb-2`}>
+          Lists
+        </Text>
 
         <SidebarButton
           onPress={() => editor?.chain().focus().toggleBulletList().run()}
@@ -112,7 +125,7 @@ export default function EditorSidebar({
             isActive={editor?.isActive("bulletList") || false}
             name="list-unordered"
           />
-          <Text>Bullet-List</Text>
+          <Text variant="small">Bullet-List</Text>
         </SidebarButton>
 
         <SidebarButton
@@ -122,7 +135,7 @@ export default function EditorSidebar({
             isActive={editor?.isActive("orderedList") || false}
             name="list-ordered"
           />
-          <Text>Numbered List</Text>
+          <Text variant="small">Numbered List</Text>
         </SidebarButton>
 
         <SidebarButton
@@ -132,7 +145,7 @@ export default function EditorSidebar({
             isActive={editor?.isActive("taskList") || false}
             name="list-check-2"
           />
-          <Text>Checklist</Text>
+          <Text variant="small">Checklist</Text>
         </SidebarButton>
       </div>
     </View>
