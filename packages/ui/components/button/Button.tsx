639e4c5bcb552304731a5b39ca05aa59e235813a
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-05-17 17:20:37 +0200

    add small and primary/secondary distinction and stylings to Button component

--- packages/ui/components/button/Button.tsx
+++ packages/ui/components/button/Button.tsx
@@ -11,13 +11,22 @@ import { useFocusRing } from "@react-native-aria/focus";
 import { Pressable } from "native-base";
 import { tw } from "../../tailwind";
 
-interface Props extends PressableProps {}
+export type ButtonVariants = "primary" | "secondary";
+export type ButtonSizes = "small" | "regular";
+
+export type ButtonProps = PressableProps & {
+  size?: ButtonSizes;
+  variant?: ButtonVariants;
+};
+
 type ComputeStyleParams = {
   disabled?: boolean | null;
   isPressed: boolean;
   isHovered: boolean;
   isFocusVisible: boolean;
   isFocused: boolean;
+  variant?: ButtonVariants | null;
+  size?: ButtonSizes | null;
 };
 
 const computeStyle = ({
@@ -25,23 +34,82 @@ const computeStyle = ({
   isPressed,
   isHovered,
   isFocusVisible,
+  variant,
+  size,
 }: ComputeStyleParams) => {
-  if (disabled) return tw`bg-gray-300`;
+  if (disabled) {
+    switch (variant) {
+      case "secondary":
+        return tw`bg-gray-100 border-gray-300`;
+      case "primary":
+      default:
+        return tw`bg-gray-300 border-gray-300`;
+    }
+  }
 
   let style: any = isFocusVisible
-    ? tw.style("se-outline-focus") // web only
+    ? tw.style(size === "small" ? "se-inset-focus-mini" : "se-inset-focus") // web only
     : {};
-  if (isPressed) return tw.style(`bg-primary-600`);
+
+  if (isPressed) {
+    switch (variant) {
+      case "secondary":
+        return tw`bg-primary-200 border-primary-200`;
+      case "primary":
+      default:
+        return tw`bg-primary-700 border-primary-700`;
+    }
+  }
+
   // style is merged in to make sure a focused button that is hovered still has an outline
-  if (isHovered) return tw.style(`bg-primary-700`, style);
+  if (isHovered) {
+    switch (variant) {
+      case "secondary":
+        return tw.style(`bg-primary-100`, style);
+      case "primary":
+      default:
+        return tw.style(`bg-primary-600 border-primary-600`, style);
+    }
+  }
+
   return style;
 };
 
-export const Button = forwardRef((props: Props, ref) => {
+export const Button = forwardRef((props: ButtonProps, ref) => {
   const { isFocusVisible, focusProps: focusRingProps } = useFocusRing();
+  const { variant = "primary", size = "regular" } = props;
+
+  // generic wrapper-styles
+  const wrapperStyle = {
+    size: {
+      small: tw`px-3 py-2`,
+      regular: tw`px-4 py-3`,
+    },
+    variant: {
+      primary: tw`bg-primary-500 border-primary-500`,
+      secondary: tw`bg-transparent border-primary-200`,
+    },
+  };
+
+  // generic text-styles
+  const textStyle = {
+    size: {
+      small: tw`small font-semibold`,
+      regular: tw`text-base`,
+    },
+    variant: {
+      primary: tw`text-gray-100`,
+      secondary: tw`text-primary-400`,
+    },
+  };
+
   const styles = StyleSheet.create({
-    wrapper: tw`rounded px-4 py-3 bg-primary-500`,
-    text: tw`text-base text-center text-gray-100`,
+    wrapper: tw.style(
+      `rounded border-solid border-2`,
+      wrapperStyle.size[size],
+      wrapperStyle.variant[variant]
+    ),
+    text: tw`text-center`,
   });
 
   return (
@@ -68,12 +136,23 @@ export const Button = forwardRef((props: Props, ref) => {
                 isHovered,
                 isFocusVisible,
                 isFocused,
+                variant: props.variant,
+                size: props.size,
               }),
               { cursor: props.disabled ? "not-allowed" : "pointer" }, // web only
               props.style,
             ]}
           >
-            <RnText style={styles.text}>{props.children}</RnText>
+            <RnText
+              style={[
+                styles.text,
+                textStyle.size[size],
+                textStyle.variant[variant],
+                props.disabled && variant === "secondary" && tw`text-gray-400`,
+              ]}
+            >
+              {props.children}
+            </RnText>
           </RnView>
         );
       }}
