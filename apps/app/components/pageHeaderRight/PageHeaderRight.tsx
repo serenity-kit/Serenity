4be76e326c5a432662ae20c9bfbedeea769e67cd
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-05-19 15:58:43 +0200

    Add styling and dummy-elements to Right PageHeader

--- apps/app/components/pageHeaderRight/PageHeaderRight.tsx
+++ apps/app/components/pageHeaderRight/PageHeaderRight.tsx
@@ -1,5 +1,6 @@
 import { useHasEditorSidebar } from "@serenity-tools/editor/hooks/useHasEditorSidebar";
-import { Text, tw, View } from "@serenity-tools/ui";
+import { Text, tw, Avatar, AvatarGroup, Button } from "@serenity-tools/ui";
+import { HStack } from "native-base";
 import React from "react";
 
 export function PageHeaderRight() {
@@ -10,8 +11,19 @@ export function PageHeaderRight() {
   }
 
   return (
-    <View style={tw`h-full w-60 border-l border-b border-gray-200 bg-gray-100`}>
-      <Text>Desktop Header Right</Text>
-    </View>
+    <HStack
+      style={tw`h-full w-60 px-3 border-l border-b border-gray-200 bg-gray-100`}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <AvatarGroup max={3} _avatar={{ size: "sm" }}>
+        <Avatar bg="green.400">BE</Avatar>
+        <Avatar bg="yellow.400">AN</Avatar>
+        <Avatar bg="orange.400">NG</Avatar>
+        <Avatar bg="yellow.300">NG</Avatar>
+        <Avatar bg="green.300">NG</Avatar>
+      </AvatarGroup>
+      <Button size="small">Share</Button>
+    </HStack>
   );
 }
