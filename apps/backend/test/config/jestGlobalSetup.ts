4d429d1f6ca4734821d942c4888659212f9228af
Nik Graf <nik@nikgraf.com>
2022-06-13 10:05:10 +0200

    fix jest setup

---
+++ apps/backend/test/config/jestGlobalSetup.ts
@@ -0,0 +1,28 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+module.exports = async function (globalConfig, projectConfig) {
+  const databaseName = "serenity_test";
+  const connectionString = `postgres://prisma:prisma@localhost:5432/${databaseName}`;
+  const prismaBinary = "./node_modules/.bin/prisma";
+  // Set the required environment variable to contain the connection string
+  // to our database
+  process.env.POSTGRES_URL = connectionString;
+  global.process.env.POSTGRES_URL = connectionString;
+
+  // seems to work better than DROP DATABSE
+  await exec(
+    `POSTGRES_URL=${connectionString} ${prismaBinary} migrate reset -f`
+  );
+
+  // Run the migrations to ensure our schema has the required structure
+  await exec(`POSTGRES_URL=${connectionString} ${prismaBinary} migrate deploy`);
+  // Regenerate client
+  await exec(`POSTGRES_URL=${connectionString} ${prismaBinary} generate`);
+};
+
+// afterAll(() => {
+//   // disconnect Prisma from the database after all tests are complete
+//   // to avoid open handles stopping Jest from exiting
+//   prisma.$disconnect();
+// });
