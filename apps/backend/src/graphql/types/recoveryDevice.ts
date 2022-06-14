8973e5b84b280ba3cb9029ea46eaf46fd03d88a6
Nik Graf <nik@nikgraf.com>
2022-06-14 14:26:45 +0200

    cleanup recovery device type

--- apps/backend/src/graphql/types/recoveryDevice.ts
+++ apps/backend/src/graphql/types/recoveryDevice.ts
@@ -3,15 +3,8 @@ import { objectType } from "nexus";
 export const RecoveryDevice = objectType({
   name: "RecoveryDevice",
   definition(t) {
-    t.nonNull.string("userId");
-    t.nonNull.string("ciphertext");
-    t.nonNull.string("nonce");
     t.nonNull.string("deviceSigningPublicKey");
-    t.string("deviceSigningPrivateKey");
-    t.nonNull.string("deviceSigningKeyType");
     t.nonNull.string("deviceEncryptionPublicKey");
-    t.string("deviceEncryptionPrivateKey");
-    t.nonNull.string("deviceEncryptionKeyType");
     t.nonNull.string("signatureForMainDeviceSigningPublicKey");
     t.nonNull.string("signatureForRecoveryDeviceSigningPublicKey");
   },
--- apps/backend/src/types/recoveryDevice.ts
+++ apps/backend/src/types/recoveryDevice.ts
@@ -1,13 +1,6 @@
 export type RecoveryDevice = {
-  userId: string;
-  ciphertext: string;
-  nonce: string;
   deviceSigningPublicKey: string;
-  deviceSigningPrivateKey?: string;
-  deviceSigningKeyType: string;
-  deviceEncryptionPublicKey?: string;
-  deviceEncryptionPrivateKey?: string;
-  deviceEncryptionKeyType?: string;
+  deviceEncryptionPublicKey: string;
   signatureForMainDeviceSigningPublicKey: string;
   signatureForRecoveryDeviceSigningPublicKey: string;
 };
