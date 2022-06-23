7c7effb7cb469515f631a054e8ca125a1830b30e
Nik Graf <nik@nikgraf.com>
2022-06-23 17:16:39 +0200

    restore createWorkspaceInvitation tests

--- apps/backend/src/graphql/mutations/workspace/createWorkspaceInvitation.test.ts
+++ apps/backend/src/graphql/mutations/workspace/createWorkspaceInvitation.test.ts
@@ -31,45 +31,45 @@ test("user should be able to create an invitation", async () => {
     workspaceId,
     authorizationHeader: userAndDevice.device.signingPublicKey,
   });
-  // const workspaceInvitation =
-  //   result.createWorkspaceInvitation.workspaceInvitation;
-  // expect(typeof workspaceInvitation.id).toBe("string");
-  // expect(workspaceInvitation.workspaceId).toBe(workspaceId);
-  // expect(workspaceInvitation.inviterUserId).toBe(userAndDevice.user.id);
-  // expect(workspaceInvitation.expiresAt).toBeDefined();
+  const workspaceInvitation =
+    result.createWorkspaceInvitation.workspaceInvitation;
+  expect(typeof workspaceInvitation.id).toBe("string");
+  expect(workspaceInvitation.workspaceId).toBe(workspaceId);
+  expect(workspaceInvitation.inviterUserId).toBe(userAndDevice.user.id);
+  expect(workspaceInvitation.expiresAt).toBeDefined();
 });
 
-// test("user should not be able to invite from a workspace they don't own", async () => {
-//   const workspaceId1 = "workspace2";
-//   const workspaceId2 = "otherWorkspace";
-//   const username1 = "adam@example.com";
-//   const username2 = "betty@example.com";
-//   const userAndDevice1 = await createUserWithWorkspace({
-//     id: workspaceId1,
-//     username: username1,
-//   });
-//   userAndDevice2 = await createUserWithWorkspace({
-//     id: workspaceId2,
-//     username: username2,
-//   });
-//   await expect(
-//     (async () =>
-//       await createWorkspaceInvitation({
-//         graphql,
-//         workspaceId: workspaceId2,
-//         authorizationHeader: userAndDevice1.device.signingPublicKey,
-//       }))()
-//   ).rejects.toThrow("Unauthorized");
-// });
+test("user should not be able to invite from a workspace they don't own", async () => {
+  const workspaceId1 = "workspace2";
+  const workspaceId2 = "otherWorkspace";
+  const username1 = "adam@example.com";
+  const username2 = "betty@example.com";
+  const userAndDevice1 = await createUserWithWorkspace({
+    id: workspaceId1,
+    username: username1,
+  });
+  userAndDevice2 = await createUserWithWorkspace({
+    id: workspaceId2,
+    username: username2,
+  });
+  await expect(
+    (async () =>
+      await createWorkspaceInvitation({
+        graphql,
+        workspaceId: workspaceId2,
+        authorizationHeader: userAndDevice1.device.signingPublicKey,
+      }))()
+  ).rejects.toThrow("Unauthorized");
+});
 
-// test("user should not be able to invite from a workspace that doesn't exist", async () => {
-//   const username = "adam@example.com";
-//   await expect(
-//     (async () =>
-//       await createWorkspaceInvitation({
-//         graphql,
-//         workspaceId: "nonexistantWorkspace",
-//         authorizationHeader: userAndDevice2.device.signingPublicKey,
-//       }))()
-//   ).rejects.toThrow("Unauthorized");
-// });
+test("user should not be able to invite from a workspace that doesn't exist", async () => {
+  const username = "adam@example.com";
+  await expect(
+    (async () =>
+      await createWorkspaceInvitation({
+        graphql,
+        workspaceId: "nonexistantWorkspace",
+        authorizationHeader: userAndDevice2.device.signingPublicKey,
+      }))()
+  ).rejects.toThrow("Unauthorized");
+});
