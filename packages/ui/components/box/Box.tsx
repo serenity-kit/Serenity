f15a006ccf669f1893a29276c33f460f7743ed21
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-06-29 19:55:01 +0200

    Export BoxProps of Box component

--- packages/ui/components/box/Box.tsx
+++ packages/ui/components/box/Box.tsx
@@ -3,7 +3,9 @@ import { StyleSheet } from "react-native";
 import { Box as NbBox, IBoxProps, VStack } from "native-base";
 import { tw } from "../../tailwind";
 
-export const Box = forwardRef((props: IBoxProps, ref) => {
+export type BoxProps = IBoxProps;
+
+export const Box = forwardRef((props: BoxProps, ref) => {
   const styles = StyleSheet.create({
     // max-width set as we use boxes mainly as dialogs yet and they should be uniform in the whole application
     box: tw`max-w-md w-full bg-white text-base text-gray-900 px-6 py-12 sm:px-12 rounded-xl`,
