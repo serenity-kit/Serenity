cdfc783c010f8fcd486e31182993019a3fcdc88c
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-06-30 14:56:11 +0200

    Use native Pressable for SidebarLink

--- packages/ui/components/sidebarLink/SidebarLink.tsx
+++ packages/ui/components/sidebarLink/SidebarLink.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { Platform } from "react-native";
+import { Platform, Pressable } from "react-native";
 import { tw } from "../../tailwind";
-import { Link as ReactNavigationLink } from "@react-navigation/native";
+import { useLinkProps } from "@react-navigation/native";
 import type { NavigationAction } from "@react-navigation/core";
 import { GestureResponderEvent, TextProps, StyleSheet } from "react-native";
 import { To } from "@react-navigation/native/lib/typescript/src/useLinkTo";
@@ -26,33 +26,33 @@ export function SidebarLink<ParamList extends ReactNavigation.RootParamList>(
 ) {
   const [isHovered, setIsHovered] = React.useState(false);
   const { isFocusVisible, focusProps: focusRingProps }: any = useFocusRing();
+  const linkProps = useLinkProps({
+    ...props,
+  });
 
   const styles = StyleSheet.create({
-    default: tw.style(
-      `px-4 py-1.5`,
-      Platform.OS === "web" && { outlineWidth: 0 }
-    ),
+    link: tw.style(Platform.OS === "web" && { outlineWidth: 0 }),
+    stack: tw.style(`flex px-4 py-1.5`), // flex needed for correct height calculation
     hover: tw`bg-gray-200`,
     focusVisible: Platform.OS === "web" ? tw`se-inset-focus-mini` : {},
   });
 
   return (
-    <ReactNavigationLink
+    <Pressable
+      {...linkProps}
       {...props}
       {...focusRingProps} // sets onFocus and onBlur
       style={[
-        styles.default,
+        styles.link,
         isHovered && styles.hover,
         isFocusVisible && styles.focusVisible,
-        props.style,
       ]}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      {/* flex needed for correct height calculation */}
-      <HStack space={2} alignItems="center" style={tw`flex`}>
+      <HStack space={2} alignItems="center" style={[styles.stack, props.style]}>
         {props.children}
       </HStack>
-    </ReactNavigationLink>
+    </Pressable>
   );
 }
