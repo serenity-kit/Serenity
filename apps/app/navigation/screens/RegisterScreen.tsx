fd9ea781ca5a83174499962c8b5d0be72c3c9176
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-06-30 16:50:57 +0200

    Add SafeArea- and KeyboardAvoidingViews to Login and Register

--- apps/app/navigation/screens/RegisterScreen.tsx
+++ apps/app/navigation/screens/RegisterScreen.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Text, View, Box, tw } from "@serenity-tools/ui";
 import { RootStackScreenProps } from "../../types";
 import RegisterForm from "../../components/register/RegisterForm";
+import { KeyboardAvoidingView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RegisterScreen(
   props: RootStackScreenProps<"Register">
@@ -14,22 +16,26 @@ export default function RegisterScreen(
   };
 
   return (
-    <View
-      style={tw`bg-white xs:bg-primary-900 justify-center items-center flex-auto`}
-    >
-      <Box>
-        <View>
-          <Text variant="large" bold style={tw`text-center`}>
-            Create your Account
-          </Text>
-          <Text muted style={tw`text-center`}>
-            Sign up and start your free trial!
-            {"\n"}
-            No credit card required.
-          </Text>
+    <SafeAreaView style={tw`flex-auto`}>
+      <KeyboardAvoidingView behavior="padding" style={tw`flex-auto`}>
+        <View
+          style={tw`bg-white xs:bg-primary-900 justify-center items-center flex-auto`}
+        >
+          <Box>
+            <View>
+              <Text variant="large" bold style={tw`text-center`}>
+                Create your Account
+              </Text>
+              <Text muted style={tw`text-center`}>
+                Sign up and start your free trial!
+                {"\n"}
+                No credit card required.
+              </Text>
+            </View>
+            <RegisterForm onRegisterSuccess={onRegisterSuccess} />
+          </Box>
         </View>
-        <RegisterForm onRegisterSuccess={onRegisterSuccess} />
-      </Box>
-    </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
