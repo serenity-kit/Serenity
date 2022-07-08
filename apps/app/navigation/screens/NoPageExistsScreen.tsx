8533f1b6c23c273f14d7078dc6c5a7a253df4b60
Adonis Gaitatzis <adonis@Adoniss-MacBook-Pro-2.local>
2022-07-08 18:39:53 +0200

    adding page not found screen

--- apps/app/navigation/screens/NoPageExistsScreen.tsx
+++ apps/app/navigation/screens/NoPageExistsScreen.tsx
@@ -1,8 +1,51 @@
-import { View } from "@serenity-tools/ui";
-import { useWindowDimensions } from "react-native";
+import { View, Text } from "@serenity-tools/ui";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import { useWorkspaceId } from "../../context/WorkspaceIdContext";
 
 export default function NoPageExistsScreen(props) {
   useWindowDimensions(); // needed to ensure tw-breakpoints are triggered when resizing
+  const workspaceId = useWorkspaceId();
 
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This page doesn't exist.</Text>
+      <Text>It has been removed or you no longer have access.</Text>
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.replace("Workspace", {
+            screen: "WorkspaceRoot",
+            workspaceId,
+          });
+        }}
+        style={styles.link}
+      >
+        <Text style={styles.linkText}>Go to workspace!</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: "#2e78b7",
+  },
+});
--- apps/app/navigation/screens/PageScreen.tsx
+++ apps/app/navigation/screens/PageScreen.tsx
@@ -8,11 +8,37 @@ import { PageHeader } from "../../components/page/PageHeader";
 import { setLastUsedDocumentId } from "../../utils/lastUsedWorkspaceAndDocumentStore/lastUsedWorkspaceAndDocumentStore";
 import { useWorkspaceId } from "../../context/WorkspaceIdContext";
 import { useDocumentStore } from "../../utils/document/documentStore";
+import {
+  DocumentQuery,
+  DocumentQueryVariables,
+  DocumentDocument,
+} from "../../generated/graphql";
+import { useClient } from "urql";
 
 export default function PageScreen(props: WorkspaceDrawerScreenProps<"Page">) {
   useWindowDimensions(); // needed to ensure tw-breakpoints are triggered when resizing
   const workspaceId = useWorkspaceId();
   const documentStore = useDocumentStore();
+  const pageId = props.route.params.pageId;
+  const urqlClient = useClient();
+
+  const doesUserHaveAccess = async (docId: string) => {
+    const documentResult = await urqlClient
+      .query<DocumentQuery, DocumentQueryVariables>(
+        DocumentDocument,
+        { id: docId },
+        {
+          // better to be safe here and always refetch
+          requestPolicy: "network-only",
+        }
+      )
+      .toPromise();
+    console.log({ documentResult });
+    if (documentResult.error?.message === "[GraphQL] Document not found") {
+      return false;
+    }
+    return true;
+  };
 
   useLayoutEffect(() => {
     props.navigation.setOptions({
@@ -25,7 +51,7 @@ export default function PageScreen(props: WorkspaceDrawerScreenProps<"Page">) {
   const updateTitle = async (title: string) => {
     const updateDocumentNameResult = await updateDocumentNameMutation({
       input: {
-        id: props.route.params.pageId,
+        id: pageId,
         name: title,
       },
     });
@@ -37,17 +63,28 @@ export default function PageScreen(props: WorkspaceDrawerScreenProps<"Page">) {
   };
 
   useEffect(() => {
-    setLastUsedDocumentId(props.route.params.pageId, workspaceId);
-
-    // removing the isNew param right after the first render so users don't have it after a refresh
+    setLastUsedDocumentId(pageId, workspaceId);
     props.navigation.setParams({ isNew: undefined });
-  }, [props.route.params.pageId]);
+    (async () => {
+      if (pageId) {
+        const hasAccess = await doesUserHaveAccess(pageId);
+        console.log({ hasAccess });
+        if (!hasAccess) {
+          props.navigation.replace("Workspace", {
+            workspaceId,
+            screen: "NoPageExists",
+          });
+        }
+      }
+      // removing the isNew param right after the first render so users don't have it after a refresh
+    })();
+  }, [pageId]);
 
   return (
     <Page
       {...props}
       // to force unmount and mount the page
-      key={props.route.params.pageId}
+      key={pageId}
       updateTitle={updateTitle}
     />
   );
