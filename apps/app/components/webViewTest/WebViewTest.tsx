6b98a27018fc824e102ac51a68c37fcd94cc384a
Nik Graf <nik@nikgraf.com>
2022-07-14 13:08:37 +0200

    another mime type incorrect when trying to load the wasm file

--- apps/app/components/webViewTest/WebViewTest.tsx
+++ apps/app/components/webViewTest/WebViewTest.tsx
@@ -5,11 +5,12 @@ import { WebView } from "react-native-webview";
 let source = Platform.OS === "ios" ? require("./index.html") : { html: null };
 
 export default function WebViewTest() {
-  const webViewRef = useRef<WebView>(null);
+  return null;
+  // const webViewRef = useRef<WebView>(null);
 
-  return (
-    <View style={{ height: 0, width: 0, overflow: "hidden" }}>
-      <WebView ref={webViewRef} originWhitelist={["*"]} source={source} />
-    </View>
-  );
+  // return (
+  //   <View style={{ height: 0, width: 0, overflow: "hidden" }}>
+  //     <WebView ref={webViewRef} originWhitelist={["*"]} source={source} />
+  //   </View>
+  // );
 }
--- packages/opaque/opaque-mobile-webview-script.ts
+++ packages/opaque/opaque-mobile-webview-script.ts
@@ -1,59 +1,63 @@
+alert("BB");
+
 import { Registration, Login } from "opaque-wasm";
 
-const toBase64 = (data: Uint8Array) => {
-  return btoa(String.fromCharCode.apply(null, [...data]));
-};
+// const toBase64 = (data: Uint8Array) => {
+//   return btoa(String.fromCharCode.apply(null, [...data]));
+// };
 
-const fromBase64 = (value: string) => {
-  return Uint8Array.from(atob(value), (c) => c.charCodeAt(0));
-};
+// const fromBase64 = (value: string) => {
+//   return Uint8Array.from(atob(value), (c) => c.charCodeAt(0));
+// };
 
 const registration = new Registration();
-const login = new Login();
+// const login = new Login();
+
+// alert("register on window");
 
 window.registerInitialize = function (id: string, password: string) {
-  const message = registration.start(password);
-  window.ReactNativeWebView.postMessage(
-    JSON.stringify({
-      id,
-      result: toBase64(message),
-    })
-  );
+  // const message = registration.start(password);
+  // window.ReactNativeWebView.postMessage(
+  //   JSON.stringify({
+  //     id,
+  //     result: toBase64(message),
+  //   })
+  // );
 };
 
-window.finishRegistration = function (id: string, challengeResponse: string) {
-  const message = registration.finish(fromBase64(challengeResponse));
-  window.ReactNativeWebView.postMessage(
-    JSON.stringify({
-      id,
-      result: {
-        exportKey: toBase64(registration.getExportKey()),
-        response: toBase64(message),
-      },
-    })
-  );
-};
+// window.finishRegistration = function (id: string, challengeResponse: string) {
+//   // const message = registration.finish(fromBase64(challengeResponse));
+//   // window.ReactNativeWebView.postMessage(
+//   //   JSON.stringify({
+//   //     id,
+//   //     result: {
+//   //       exportKey: toBase64(registration.getExportKey()),
+//   //       response: toBase64(message),
+//   //     },
+//   //   })
+//   // );
+// };
 
-window.startLogin = function (id: string, password: string) {
-  const message = login.start(password);
-  window.ReactNativeWebView.postMessage(
-    JSON.stringify({
-      id,
-      result: toBase64(message),
-    })
-  );
-};
+// window.startLogin = function (id: string, password: string) {
+//   // const message = login.start(password);
+//   // window.ReactNativeWebView.postMessage(
+//   //   JSON.stringify({
+//   //     id,
+//   //     result: toBase64(message),
+//   //   })
+//   // );
+// };
 
-window.finishLogin = function (id: string, response: string) {
-  const message = login.finish(fromBase64(response));
-  window.ReactNativeWebView.postMessage(
-    JSON.stringify({
-      id,
-      result: {
-        sessionKey: toBase64(login.getSessionKey()),
-        exportKey: toBase64(login.getExportKey()),
-        response: toBase64(message),
-      },
-    })
-  );
-};
+// window.finishLogin = function (id: string, response: string) {
+//   // const message = login.finish(fromBase64(response));
+//   // window.ReactNativeWebView.postMessage(
+//   //   JSON.stringify({
+//   //     id,
+//   //     result: {
+//   //       sessionKey: toBase64(login.getSessionKey()),
+//   //       exportKey: toBase64(login.getExportKey()),
+//   //       response: toBase64(message),
+//   //     },
+//   //   })
+//   // );
+// };
