e4320d4c370dca2913c051974a1a0a2056e8d2c7
Adonis Gaitatzis <adonis@Adoniss-MacBook-Pro-2.local>
2022-07-15 14:38:34 +0200

    adding playwright end-to-end testing

---
+++ apps/backend/playwright.config.ts
@@ -0,0 +1,17 @@
+import { type PlaywrightTestConfig, devices } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+  testMatch: /.*\.e2e\.ts/,
+};
+export default config;
