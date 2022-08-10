df3962e9d795d5aa64369f6d5083883d24113967
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-08-10 12:03:49 +0200

    Add plush option to Box component for easier unification

--- packages/ui/components/box/Box.tsx
+++ packages/ui/components/box/Box.tsx
@@ -3,12 +3,15 @@ import { StyleSheet } from "react-native";
 import { Box as NbBox, IBoxProps, VStack } from "native-base";
 import { tw } from "../../tailwind";
 
-export type BoxProps = IBoxProps;
+export type BoxProps = IBoxProps & {
+  plush?: boolean;
+};
 
 export const Box = forwardRef((props: BoxProps, ref) => {
+  const padding = props.plush ? `px-6 py-12 sm:px-12` : `p-6`;
   const styles = StyleSheet.create({
     // max-width set as we use boxes mainly as dialogs yet and they should be uniform in the whole application
-    box: tw`max-w-md w-full bg-white text-base text-gray-900 px-6 py-12 sm:px-12 rounded-xl`,
+    box: tw`max-w-md w-full bg-white text-base text-gray-900 ${padding} rounded-xl`,
   });
 
   return (
