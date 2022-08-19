f8ea7795f03d1aa2a0a51bc6bf69bb4e371c7281
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-08-19 22:10:40 +0200

    Add vertical option to ExampleArea

--- packages/ui/components/designSystem/DesignSystemExampleArea.tsx
+++ packages/ui/components/designSystem/DesignSystemExampleArea.tsx
@@ -1,23 +1,30 @@
-import { HStack } from "native-base";
+import { Stack } from "native-base";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { tw } from "../../tailwind";
 import { View, ViewProps } from "../view/View";
 
-export type DesignSystemExampleAreaProps = ViewProps & {};
+export type DesignSystemExampleAreaProps = ViewProps & {
+  vertical?: boolean;
+};
 
 export const DesignSystemExampleArea = (
   props: DesignSystemExampleAreaProps
 ) => {
+  const { vertical } = props;
   const styles = StyleSheet.create({
     area: tw`mt-2.5 p-4 border border-gray-200 rounded overflow-scroll sm:overflow-visible items-start`,
   });
 
   return (
     <View {...props} style={[styles.area, props.style]}>
-      <HStack space={4} alignItems={"center"}>
+      <Stack
+        direction={vertical ? "column" : "row"}
+        space={4}
+        alignItems={"center"}
+      >
         {props.children}
-      </HStack>
+      </Stack>
     </View>
   );
 };
