c6ddd2ce8228da215f30daf0c0513cce4ff4c1df
Nik Graf <nik@nikgraf.com>
2022-09-04 10:39:39 +0200

    add back button to onboarding screens

--- apps/app/navigation/screens/LoginScreen.tsx
+++ apps/app/navigation/screens/LoginScreen.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { View, tw, Box, Text, Link, CenterContent } from "@serenity-tools/ui";
-import { RootStackScreenProps } from "../../types/navigation";
+import { Box, Link, Text, tw, View } from "@serenity-tools/ui";
 import { LoginForm } from "../../components/login/LoginForm";
+import { OnboardingScreenWrapper } from "../../components/onboardingScreenWrapper/OnboardingScreenWrapper";
+import { RootStackScreenProps } from "../../types/navigation";
 import { navigateToNextAuthenticatedPage } from "../../utils/authentication/loginHelper";
-import { KeyboardAvoidingView } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginScreen(props: RootStackScreenProps<"Login">) {
   const onLoginSuccess = async () => {
@@ -15,32 +13,28 @@ export default function LoginScreen(props: RootStackScreenProps<"Login">) {
   };
 
   return (
-    <SafeAreaView style={tw`flex-auto`}>
-      <KeyboardAvoidingView behavior="padding" style={tw`flex-auto`}>
-        <CenterContent serenityBg>
-          <Box plush>
-            <View>
-              <Text variant="lg" bold style={tw`text-center`}>
-                Welcome back
-              </Text>
-              <View>
-                <Text muted style={tw`text-center`}>
-                  Log in to your Serenity Account
-                </Text>
-              </View>
-            </View>
-            <LoginForm onLoginSuccess={onLoginSuccess} />
-            <View style={tw`text-center`}>
-              <Text variant="xs" muted>
-                Don't have an account?
-              </Text>
-              <Text variant="xs">
-                <Link to={{ screen: "Register" }}>Register here</Link>
-              </Text>
-            </View>
-          </Box>
-        </CenterContent>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <OnboardingScreenWrapper>
+      <Box plush>
+        <View>
+          <Text variant="lg" bold style={tw`text-center`}>
+            Welcome back
+          </Text>
+          <View>
+            <Text muted style={tw`text-center`}>
+              Log in to your Serenity Account
+            </Text>
+          </View>
+        </View>
+        <LoginForm onLoginSuccess={onLoginSuccess} />
+        <View style={tw`text-center`}>
+          <Text variant="xs" muted>
+            Don't have an account?
+          </Text>
+          <Text variant="xs">
+            <Link to={{ screen: "Register" }}>Register here</Link>
+          </Text>
+        </View>
+      </Box>
+    </OnboardingScreenWrapper>
   );
 }
