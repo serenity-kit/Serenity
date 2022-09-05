56cbe733a93a507a0af8bf93b02becbe36a6eeed
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-09-05 17:28:00 +0200

    Adjust PageHeaders for editing Mode

--- apps/app/components/page/PageHeader.tsx
+++ apps/app/components/page/PageHeader.tsx
@@ -3,6 +3,7 @@ import { Icon, Text, tw, useHasEditorSidebar } from "@serenity-tools/ui";
 import { HStack } from "native-base";
 import { useDocumentPathStore } from "../../utils/document/documentPathStore";
 import { useDocumentStore } from "../../utils/document/documentStore";
+import { useEditorStore } from "../../utils/editorStore/editorStore";
 
 type Props = DrawerHeaderProps & {
   children: any;
@@ -14,40 +15,51 @@ export function PageHeader(props: Props) {
   const document = useDocumentStore((state) => state.document);
   const documentName = useDocumentStore((state) => state.documentName);
   const hasEditorSidebar = useHasEditorSidebar();
+  const isInEditingMode = useEditorStore((state) => state.isInEditingMode);
 
   return (
-    <HStack space={0.5} alignItems="center">
-      {hasEditorSidebar ? (
-        documentPathList.map((folder) => (
-          <HStack key={folder.id} alignItems="center" space={0.5}>
+    <>
+      {isInEditingMode ? (
+        <HStack alignItems={"center"}>
+          <Text variant="xs" muted>
+            Editing mode
+          </Text>
+        </HStack>
+      ) : (
+        <HStack space={0.5} alignItems="center">
+          {hasEditorSidebar ? (
+            documentPathList.map((folder) => (
+              <HStack key={folder.id} alignItems="center" space={0.5}>
+                <Text
+                  variant="xxs"
+                  muted
+                  style={tw`max-w-30 2xl:max-w-50`}
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                >
+                  {getName(folder.id)}
+                </Text>
+                <Icon name="arrow-right-s-line" color={"gray-600"} />
+              </HStack>
+            ))
+          ) : (
+            <Text variant="xxs" muted>
+              ... /
+            </Text>
+          )}
+          {document ? (
             <Text
               variant="xxs"
-              muted
-              style={tw`max-w-30 2xl:max-w-50`}
+              bold={hasEditorSidebar}
+              style={tw`max-w-35 sm:max-w-40 md:max-w-50 2xl:max-w-70`}
               numberOfLines={1}
               ellipsizeMode="tail"
             >
-              {getName(folder.id)}
+              {documentName}
             </Text>
-            <Icon name="arrow-right-s-line" color={"gray-600"} />
-          </HStack>
-        ))
-      ) : (
-        <Text variant="xxs" muted>
-          ... /
-        </Text>
+          ) : null}
+        </HStack>
       )}
-      {document ? (
-        <Text
-          variant="xxs"
-          bold={hasEditorSidebar}
-          style={tw`max-w-35 sm:max-w-40 md:max-w-50 2xl:max-w-70`}
-          numberOfLines={1}
-          ellipsizeMode="tail"
-        >
-          {documentName}
-        </Text>
-      ) : null}
-    </HStack>
+    </>
   );
 }
--- apps/app/components/pageHeaderLeft/PageHeaderLeft.tsx
+++ apps/app/components/pageHeaderLeft/PageHeaderLeft.tsx
@@ -1,20 +1,38 @@
 import { tw, View, IconButton, useHasEditorSidebar } from "@serenity-tools/ui";
 import React from "react";
 import { DrawerActions } from "@react-navigation/native";
+import { useEditorStore } from "../../utils/editorStore/editorStore";
+import { HStack } from "native-base";
 
 export function PageHeaderLeft(props: any) {
   const hasEditorSidebar = useHasEditorSidebar();
+  const isInEditingMode = useEditorStore((state) => state.isInEditingMode);
+
+  // TODO styling depending on if going back/forward is possible
+  const actionIsPossible = true;
 
   return (
     <View style={[tw`pl-3`]}>
-      <IconButton
-        onPress={() => {
-          props.navigation.dispatch(DrawerActions.openDrawer());
-        }}
-        name="double-arrow-right"
-        color={"gray-800"}
-        size={hasEditorSidebar ? "lg" : "md"}
-      />
+      {isInEditingMode ? (
+        <HStack>
+          <IconButton
+            size={"lg"}
+            name="arrow-go-back-line"
+            // TODO
+            color={actionIsPossible ? "gray-800" : "gray-400"}
+          ></IconButton>
+          <IconButton size={"lg"} name="arrow-go-forward-line"></IconButton>
+        </HStack>
+      ) : (
+        <IconButton
+          onPress={() => {
+            props.navigation.dispatch(DrawerActions.openDrawer());
+          }}
+          name="arrow-left-line"
+          color={"gray-900"}
+          size={hasEditorSidebar ? "lg" : "md"}
+        />
+      )}
     </View>
   );
 }
