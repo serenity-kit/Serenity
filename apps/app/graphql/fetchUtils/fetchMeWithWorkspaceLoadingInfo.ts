48f74b5d8963078bb6b3f41384b981022389c620
Nik Graf <nik@nikgraf.com>
2022-09-24 19:22:01 +0200

    add loadMeAndVerifyMachine

--- apps/app/graphql/fetchUtils/fetchMeWithWorkspaceLoadingInfo.ts
+++ apps/app/graphql/fetchUtils/fetchMeWithWorkspaceLoadingInfo.ts
@@ -5,7 +5,9 @@ import {
 } from "../../generated/graphql";
 import { urqlClient } from "../../utils/urqlClient/urqlClient";
 
-export const fetchMeWithWorkspaceLoadingInfo = async (variables) => {
+export const fetchMeWithWorkspaceLoadingInfo = async (
+  variables: MeWithWorkspaceLoadingInfoQueryVariables
+) => {
   const result = await urqlClient
     .query<
       MeWithWorkspaceLoadingInfoQuery,
---
+++ apps/app/machines/loadMeAndVerifyMachine.typegen.ts
@@ -0,0 +1,45 @@
+// This file was automatically generated. Edits will be overwritten
+
+export interface Typegen0 {
+  "@@xstate/typegen": true;
+  internalEvents: {
+    "": { type: "" };
+    "done.invoke.fetchMe": {
+      type: "done.invoke.fetchMe";
+      data: unknown;
+      __tip: "See the XState TS docs to learn how to strongly type this.";
+    };
+    "error.platform.fetchMe": { type: "error.platform.fetchMe"; data: unknown };
+    "xstate.after(2000)#loadInitialData.failure": {
+      type: "xstate.after(2000)#loadInitialData.failure";
+    };
+    "xstate.init": { type: "xstate.init" };
+  };
+  invokeSrcNameMap: {
+    fetchMe: "done.invoke.fetchMe";
+  };
+  missingImplementations: {
+    actions: never;
+    services: never;
+    guards: never;
+    delays: never;
+  };
+  eventsCausingActions: {
+    redirectToLogin: "";
+  };
+  eventsCausingServices: {
+    fetchMe: "xstate.after(2000)#loadInitialData.failure" | "xstate.init";
+  };
+  eventsCausingGuards: {
+    hasNoNetworkError: "done.invoke.fetchMe";
+    isValidSession: "";
+  };
+  eventsCausingDelays: {};
+  matchesStates:
+    | "failure"
+    | "invalidSession"
+    | "loaded"
+    | "loading"
+    | "validSession";
+  tags: never;
+}
