a2f994aa403883ee39564f5477617796531e72e4
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-10-05 16:34:01 +0200

    Add optional label to DesignSystemExampleArea

--- packages/ui/components/designSystem/DesignSystemExampleArea.tsx
+++ packages/ui/components/designSystem/DesignSystemExampleArea.tsx
@@ -1,22 +1,24 @@
-import { Stack } from "native-base";
-import { ResponsiveValue } from "native-base/src/components/types/responsiveValue";
 import React from "react";
+import { Stack } from "native-base";
 import { StyleSheet } from "react-native";
 import { tw } from "../../tailwind";
 import { View, ViewProps } from "../view/View";
+import { Mono } from "../mono/Mono";
 
 export type DesignSystemExampleAreaProps = ViewProps & {
   vertical?: boolean;
   center?: boolean;
   stackWidth?: number;
+  label?: string;
 };
 
 export const DesignSystemExampleArea = (
   props: DesignSystemExampleAreaProps
 ) => {
-  const { vertical, center } = props;
+  const { vertical, center, label } = props;
   const styles = StyleSheet.create({
     area: tw`mt-2.5 p-4 border border-gray-200 rounded overflow-scroll sm:overflow-visible items-start`,
+    label: tw`absolute top-2 right-2 px-1.5 bg-white/50 rounded-sm`,
   });
 
   return (
@@ -29,6 +31,13 @@ export const DesignSystemExampleArea = (
       >
         {props.children}
       </Stack>
+      {label ? (
+        <View style={[styles.label]}>
+          <Mono size={"xs"} style={tw`text-gray-500 leading-snug`}>
+            {label}
+          </Mono>
+        </View>
+      ) : null}
     </View>
   );
 };
