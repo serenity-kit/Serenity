c78320e42dca2e739521107cdb1d2edd50a71d6c
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-10-06 09:15:11 +0200

    Make Headings flex for block behaviour

--- packages/ui/components/heading/Heading.tsx
+++ packages/ui/components/heading/Heading.tsx
@@ -11,25 +11,25 @@ export const Heading = (props: HeadingProps) => {
   const { lvl } = props;
 
   const styles = StyleSheet.create({
+    // flex needed to make them "block" elements which can have margins
+    // (display: block not available for native)
+
     // 1.75rem (28px) - login / registration
-    1: tw.style(`text-2xl md:text-3xl text-gray-900 dark:text-white`, {
+    1: tw.style(`flex text-2xl md:text-3xl text-gray-900 dark:text-white`, {
       fontFamily: "Inter_700Bold",
     }),
     // 1.5rem (24px) - context-header web (e.g. Settings Modal)
-    2: tw.style(`text-2xl text-gray-900 dark:text-white`, {
+    2: tw.style(`flex text-2xl text-gray-900 dark:text-white`, {
       fontFamily: "Inter_600SemiBold",
     }),
     // 1rem (16px) - form-header (e.g. "Manage devices" in Settings Modal)
-    3: tw.style(`text-base text-gray-900 dark:text-white`, {
+    3: tw.style(`flex text-base text-gray-900 dark:text-white`, {
       fontFamily: "Inter_600SemiBold",
     }),
     // 0.75rem (12px) - sidebar-headers (e.g. "Folders" in Sidebar)
-    4: tw.style(
-      `text-sm md:text-xxs text-gray-900 md:text-gray-800 dark:text-white`,
-      {
-        fontFamily: "Inter_600SemiBold",
-      }
-    ),
+    4: tw.style(`flex text-sm md:text-xxs text-gray-900 dark:text-white`, {
+      fontFamily: "Inter_500Medium",
+    }),
     accessibilityOnly: tw`hidden`,
   });
 
