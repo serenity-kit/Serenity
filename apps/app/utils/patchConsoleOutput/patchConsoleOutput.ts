906ceb16c1b011415927059661ec8c8714684e26
Nik Graf <nik@nikgraf.com>
2022-10-07 17:41:38 +0200

    exclude more console errors and warnings (#425)

--- apps/app/utils/patchConsoleOutput/patchConsoleOutput.ts
+++ apps/app/utils/patchConsoleOutput/patchConsoleOutput.ts
@@ -18,6 +18,10 @@ export const patchConsoleOutput = () => {
           typeof args[1] === "string" &&
           args[1].includes(
             "Check the render method of `ForwardRef(AvatarGroup)`"
+          )) ||
+        (args[0] &&
+          args[0].includes(
+            "ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17"
           ))
       ) {
         // ignored
@@ -26,6 +30,30 @@ export const patchConsoleOutput = () => {
       }
     } catch {
       originalConsoleError(...args);
+    }
+  };
+
+  const originalConsoleWarn = console.warn;
+  console.warn = (...args) => {
+    try {
+      if (
+        (args[0] &&
+          typeof args[0] === "string" &&
+          args[0].includes(
+            "Animated: `useNativeDriver` is not supported because the native animated module is missing. Falling back to JS-based animation."
+          )) ||
+        (args[0] &&
+          typeof args[0] === "string" &&
+          args[0].includes(
+            "setNativeProps is deprecated. Please update props using React state instead."
+          ))
+      ) {
+        // ignored
+      } else {
+        originalConsoleWarn(...args);
+      }
+    } catch {
+      originalConsoleWarn(...args);
     }
   };
 };
