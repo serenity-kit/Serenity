f4f55660be47387a74ec840d4f0dc2b37bccdd75
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-10-27 15:54:16 +0200

    Refactor ListHeader to align when main Item of a List is a ListIconText

--- packages/ui/components/list/ListHeader.tsx
+++ packages/ui/components/list/ListHeader.tsx
@@ -8,8 +8,7 @@ import { useIsDesktopDevice } from "../../hooks/useIsDesktopDevice/useIsDesktopD
 
 export type ListHeaderProps = IStackProps & {
   data: Array<string>;
-  mainIsSplit?: boolean;
-  splitWidth?: string;
+  mainIsIconText?: boolean;
   mainWidth?: string;
   mainWidthMobile?: string;
 };
@@ -17,8 +16,7 @@ export type ListHeaderProps = IStackProps & {
 export const ListHeader = (props: ListHeaderProps) => {
   const {
     data,
-    mainIsSplit,
-    splitWidth = "1/4",
+    mainIsIconText = false,
     mainWidth = "1/2",
     mainWidthMobile = "1/2",
     ...rest
@@ -26,28 +24,50 @@ export const ListHeader = (props: ListHeaderProps) => {
   const isDesktopDevice = useIsDesktopDevice();
 
   const styles = StyleSheet.create({
-    mainColumn: isDesktopDevice ? tw`w-${mainWidth}` : tw`w-${mainWidthMobile}`,
     headerRow: tw`p-2 justify-between bg-gray-100`,
+    mainColumn: isDesktopDevice ? tw`w-${mainWidth}` : tw`w-${mainWidthMobile}`,
+    subsetHeader: tw`w-2/5`, // same as for ListIconText elements
   });
+
+  // if main (first) column is a ListIconText component,
+  // the first 2 header elements need to be split and rendered separately in a wrapper
+  const headers = mainIsIconText ? data.slice(2) : data;
+  const mainSubset = data.slice(0, 2);
 
   return (
     <HStack {...rest} style={styles.headerRow}>
-      {data.map((item, i) => {
-        let headerStyle = mainIsSplit ? tw`${splitWidth}` : styles.mainColumn;
-
-        if ((mainIsSplit && i <= 1) || (!mainIsSplit && i > 0)) {
-          headerStyle = tw``;
-        }
-
+      {mainIsIconText ? (
+        <HStack
+          style={[styles.mainColumn, tw`justify-between`]}
+          alignItems={"center"}
+        >
+          {mainSubset.map((item, i) => {
+            return (
+              <View
+                key={"header_subset_" + i}
+                style={[styles.subsetHeader, i > 0 && tw`text-center`]}
+              >
+                <Text variant="xxs" style={tw`uppercase`} bold>
+                  {item}
+                </Text>
+              </View>
+            );
+          })}
+        </HStack>
+      ) : null}
+      {headers.map((item, i) => {
         return (
-          <View style={headerStyle} key={"header_" + i}>
+          <View
+            style={!mainIsIconText && i === 0 && styles.mainColumn}
+            key={"header_" + i}
+          >
             <Text variant="xxs" style={tw`uppercase`} bold>
               {item}
             </Text>
           </View>
         );
       })}
-      {/* dummy the same width as the action column to ensure the right header position */}
+      {/* dummy: the same width as the action column to ensure the right header positioning */}
       <View style={tw`w-5`}>
         <Text variant="xxs" style={tw`uppercase`} bold></Text>
       </View>
