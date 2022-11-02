f5cf79dc7bcbabc12baae3699a5d6fa46847084b
backupbrain <backupbrain@gmail.com>
2022-11-02 21:48:04 +0200

    Feature/snapshot keys (#475)
    * adding snapshot keys to document-share-link
    
    * wrapping document share link creation in a prisma transaction
    
    * fixing signing public key vs encryption public key lookup bug in create document link share
    
    Co-authored-by: Nik Graf <nik@nikgraf.com>

--- apps/backend/src/graphql/mutations/document/createDocumentShareLink.test.ts
+++ apps/backend/src/graphql/mutations/document/createDocumentShareLink.test.ts
@@ -1,3 +1,4 @@
+import sodium from "@serenity-tools/libsodium";
 import { gql } from "graphql-request";
 import { v4 as uuidv4 } from "uuid";
 import { Role } from "../../../../prisma/generated/output";
@@ -11,6 +12,7 @@ const password = "password";
 let userData1: any = null;
 
 const setup = async () => {
+  await sodium.ready;
   userData1 = await createUserWithWorkspace({
     id: uuidv4(),
     username: `${uuidv4()}@example.com`,
@@ -26,6 +28,7 @@ beforeAll(async () => {
 test("create share link", async () => {
   const { encryptionPrivateKey, signingPrivateKey, ...creatorDevice } =
     userData1.webDevice;
+  const snapshotKey = await sodium.crypto_kdf_keygen();
   const documentShareLinkResponse = await createDocumentShareLink({
     graphql,
     documentId: userData1.document.id,
@@ -33,6 +36,9 @@ test("create share link", async () => {
     deviceSecretBoxCiphertext: "deviceSecretBoxCiphertext",
     deviceSecretBoxNonce: "deviceSecretBoxNonce",
     creatorDevice,
+    creatorDeviceEncryptionPrivateKey: encryptionPrivateKey,
+    receiverDevices: [creatorDevice],
+    snapshotKey,
     authorizationHeader: userData1.sessionKey,
   });
   const token = documentShareLinkResponse.createDocumentShareLink.token;
@@ -49,6 +55,7 @@ test("Invalid ownership", async () => {
     otherUser.webDevice;
   const documentId = userData1.document.id;
   const authorizationHeader = otherUser.sessionKey;
+  const snapshotKey = await sodium.crypto_kdf_keygen();
   await expect(
     (async () =>
       await createDocumentShareLink({
@@ -58,6 +65,9 @@ test("Invalid ownership", async () => {
         deviceSecretBoxCiphertext: "deviceSecretBoxCiphertext",
         deviceSecretBoxNonce: "deviceSecretBoxNonce",
         creatorDevice,
+        creatorDeviceEncryptionPrivateKey: encryptionPrivateKey,
+        receiverDevices: [creatorDevice],
+        snapshotKey,
         authorizationHeader,
       }))()
   ).rejects.toThrowError("Unauthorized");
@@ -66,6 +76,7 @@ test("Invalid ownership", async () => {
 test("Unauthenticated", async () => {
   const { encryptionPrivateKey, signingPrivateKey, ...creatorDevice } =
     userData1.webDevice;
+  const snapshotKey = await sodium.crypto_kdf_keygen();
   await expect(
     (async () =>
       await createDocumentShareLink({
@@ -75,6 +86,9 @@ test("Unauthenticated", async () => {
         deviceSecretBoxCiphertext: "deviceSecretBoxCiphertext",
         deviceSecretBoxNonce: "deviceSecretBoxNonce",
         creatorDevice,
+        creatorDeviceEncryptionPrivateKey: encryptionPrivateKey,
+        receiverDevices: [creatorDevice],
+        snapshotKey,
         authorizationHeader: "badauthheader",
       }))()
   ).rejects.toThrowError(/UNAUTHENTICATED/);
@@ -96,6 +110,7 @@ describe("Input errors", () => {
     });
     const { encryptionPrivateKey, signingPrivateKey, ...creatorDevice } =
       userData1.webDevice;
+    const snapshotKey = await sodium.crypto_kdf_keygen();
     await expect(
       (async () =>
         await graphql.client.request(
@@ -108,7 +123,9 @@ describe("Input errors", () => {
               deviceSecretBoxCiphertext: "deviceSecretBoxCiphertext",
               deviceSecretBoxNonce: "deviceSecretBoxNonce",
               creatorDevice,
-              authorizationHeader: userData1.sessionKey,
+              creatorDeviceEncryptionPrivateKey: encryptionPrivateKey,
+              receiverDevices: [creatorDevice],
+              snapshotKey,
             },
           },
           { authorization: userData1.sessionKey }
@@ -123,6 +140,7 @@ describe("Input errors", () => {
     });
     const { encryptionPrivateKey, signingPrivateKey, ...creatorDevice } =
       userData1.webDevice;
+    const snapshotKey = await sodium.crypto_kdf_keygen();
     await expect(
       (async () =>
         await graphql.client.request(
@@ -135,7 +153,9 @@ describe("Input errors", () => {
               deviceSecretBoxCiphertext: "deviceSecretBoxCiphertext",
               deviceSecretBoxNonce: "deviceSecretBoxNonce",
               creatorDevice,
-              authorizationHeader: userData1.sessionKey,
+              creatorDeviceEncryptionPrivateKey: encryptionPrivateKey,
+              receiverDevices: [creatorDevice],
+              snapshotKey,
             },
           },
           { authorization: userData1.sessionKey }
@@ -155,6 +175,7 @@ describe("Input errors", () => {
     });
     const { encryptionPrivateKey, signingPrivateKey, ...creatorDevice } =
       otherUser.webDevice;
+    const snapshotKey = await sodium.crypto_kdf_keygen();
     await expect(
       (async () =>
         await graphql.client.request(
@@ -167,7 +188,9 @@ describe("Input errors", () => {
               deviceSecretBoxCiphertext: "deviceSecretBoxCiphertext",
               deviceSecretBoxNonce: "deviceSecretBoxNonce",
               creatorDevice,
-              authorizationHeader: userData1.sessionKey,
+              creatorDeviceEncryptionPrivateKey: encryptionPrivateKey,
+              receiverDevices: [creatorDevice],
+              snapshotKey,
             },
           },
           { authorization: userData1.sessionKey }
--- apps/backend/test/helpers/document/createDocumentShareLink.ts
+++ apps/backend/test/helpers/document/createDocumentShareLink.ts
@@ -1,14 +1,49 @@
+import sodium from "@serenity-tools/libsodium";
 import { gql } from "graphql-request";
 import { Role } from "../../../prisma/generated/output";
+import { SnapshotDeviceKeyBox } from "../../../src/database/document/createDocumentShareLink";
 import { Device } from "../../../src/types/device";
 
-type Params = {
+type CreateSnapshotDeviceKeyBoxParams = {
+  receiverDevices: Device[];
+  creatorDeviceEncryptionPrivateKey: string;
+  key: string;
+};
+const createSnapshotDeviceKeyBoxes = async ({
+  receiverDevices,
+  creatorDeviceEncryptionPrivateKey,
+  key,
+}: CreateSnapshotDeviceKeyBoxParams) => {
+  const snapshotKeyBoxes: SnapshotDeviceKeyBox[] = [];
+  for (const receiverDevice of receiverDevices) {
+    const nonce = await sodium.randombytes_buf(
+      sodium.crypto_secretbox_NONCEBYTES
+    );
+    const ciphertext = await sodium.crypto_box_easy(
+      key,
+      nonce,
+      receiverDevice.encryptionPublicKey,
+      creatorDeviceEncryptionPrivateKey
+    );
+    snapshotKeyBoxes.push({
+      ciphertext,
+      nonce,
+      deviceSigningPublicKey: receiverDevice.signingPublicKey,
+    });
+  }
+  return snapshotKeyBoxes;
+};
+
+export type Params = {
   graphql: any;
   documentId: string;
   sharingRole: Role;
   deviceSecretBoxCiphertext: string;
   deviceSecretBoxNonce: string;
   creatorDevice: Device;
+  creatorDeviceEncryptionPrivateKey: string;
+  snapshotKey: string;
+  receiverDevices: Device[];
   authorizationHeader: string;
 };
 
@@ -19,11 +54,21 @@ export const createDocumentShareLink = async ({
   deviceSecretBoxCiphertext,
   deviceSecretBoxNonce,
   creatorDevice,
+  creatorDeviceEncryptionPrivateKey,
+  snapshotKey,
+  receiverDevices,
   authorizationHeader,
 }: Params) => {
   const authorizationHeaders = {
     authorization: authorizationHeader,
   };
+
+  const snapshotDeviceKeyBoxes = await createSnapshotDeviceKeyBoxes({
+    receiverDevices,
+    creatorDeviceEncryptionPrivateKey,
+    key: snapshotKey,
+  });
+
   const query = gql`
     mutation createDocumentShareLink($input: CreateDocumentShareLinkInput!) {
       createDocumentShareLink(input: $input) {
@@ -40,6 +85,7 @@ export const createDocumentShareLink = async ({
         deviceSecretBoxCiphertext,
         deviceSecretBoxNonce,
         creatorDevice,
+        snapshotDeviceKeyBoxes,
       },
     },
     authorizationHeaders
