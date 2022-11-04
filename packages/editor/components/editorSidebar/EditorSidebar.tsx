5d3b48186c0c7072eafd7d1b6ca3bb4c988e627f
Nik Graf <nik@nikgraf.com>
2022-11-04 10:04:43 +0100

    remove div from EditorSidebar (#482)

--- packages/editor/components/editorSidebar/EditorSidebar.tsx
+++ packages/editor/components/editorSidebar/EditorSidebar.tsx
@@ -33,194 +33,186 @@ export default function EditorSidebar({
     <View
       style={tw`w-sidebar h-full border-l border-gray-200 bg-gray-100 pt-4`}
     >
-      <div>
-        <Heading lvl={4} style={tw`ml-4`} padded>
-          Formats
-        </Heading>
+      <Heading lvl={4} style={tw`ml-4`} padded>
+        Formats
+      </Heading>
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleBold().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("bold") || false}
-            name="bold"
-          />
-          <Text variant="xs" bold={editor?.isActive("bold") || false}>
-            Bold
-          </Text>
-        </SidebarButton>
+      <SidebarButton onPress={() => editor?.chain().focus().toggleBold().run()}>
+        <EditorSidebarIcon
+          isActive={editor?.isActive("bold") || false}
+          name="bold"
+        />
+        <Text variant="xs" bold={editor?.isActive("bold") || false}>
+          Bold
+        </Text>
+      </SidebarButton>
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleItalic().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("italic") || false}
-            name="italic"
-          />
-          <Text variant="xs" bold={editor?.isActive("italic") || false}>
-            Italic
-          </Text>
-        </SidebarButton>
+      <SidebarButton
+        onPress={() => editor?.chain().focus().toggleItalic().run()}
+      >
+        <EditorSidebarIcon
+          isActive={editor?.isActive("italic") || false}
+          name="italic"
+        />
+        <Text variant="xs" bold={editor?.isActive("italic") || false}>
+          Italic
+        </Text>
+      </SidebarButton>
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleCode().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("code") || false}
-            name="code-view"
-          />
-          <Text variant="xs" bold={editor?.isActive("code") || false}>
-            Code
-          </Text>
-        </SidebarButton>
+      <SidebarButton onPress={() => editor?.chain().focus().toggleCode().run()}>
+        <EditorSidebarIcon
+          isActive={editor?.isActive("code") || false}
+          name="code-view"
+        />
+        <Text variant="xs" bold={editor?.isActive("code") || false}>
+          Code
+        </Text>
+      </SidebarButton>
 
-        {/* styling dummy */}
-        <SidebarButton
-          onPress={() =>
-            editor?.chain().focus().toggleLink({ href: "#" }).run()
-          }
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("link") || false}
-            name="link"
-          />
-          <Text variant="xs" bold={editor?.isActive("link") || false}>
-            Link
-          </Text>
-        </SidebarButton>
+      {/* styling dummy */}
+      <SidebarButton
+        onPress={() => editor?.chain().focus().toggleLink({ href: "#" }).run()}
+      >
+        <EditorSidebarIcon
+          isActive={editor?.isActive("link") || false}
+          name="link"
+        />
+        <Text variant="xs" bold={editor?.isActive("link") || false}>
+          Link
+        </Text>
+      </SidebarButton>
 
-        <SidebarDivider />
+      <SidebarDivider />
 
-        <Heading lvl={4} style={tw`ml-4`} padded>
-          Blocks
-        </Heading>
+      <Heading lvl={4} style={tw`ml-4`} padded>
+        Blocks
+      </Heading>
 
-        {headingLevels.map((lvl) => {
-          return (
-            <SidebarButton
-              key={lvl}
-              onPress={() =>
-                editor?.chain().focus().toggleHeading({ level: lvl }).run()
-              }
+      {headingLevels.map((lvl) => {
+        return (
+          <SidebarButton
+            key={lvl}
+            onPress={() =>
+              editor?.chain().focus().toggleHeading({ level: lvl }).run()
+            }
+          >
+            <EditorSidebarIcon
+              isActive={editor?.isActive("heading", { level: lvl }) || false}
+              name={`h-${lvl}`}
+            />
+            <Text
+              variant="xs"
+              bold={editor?.isActive("heading", { level: lvl }) || false}
             >
-              <EditorSidebarIcon
-                isActive={editor?.isActive("heading", { level: lvl }) || false}
-                name={`h-${lvl}`}
-              />
-              <Text
-                variant="xs"
-                bold={editor?.isActive("heading", { level: lvl }) || false}
-              >
-                Headline {lvl}
-              </Text>
-            </SidebarButton>
-          );
-        })}
+              Headline {lvl}
+            </Text>
+          </SidebarButton>
+        );
+      })}
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleCodeBlock().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("codeBlock") || false}
-            name="code-s-slash-line"
-          />
-          <Text variant="xs" bold={editor?.isActive("codeBlock") || false}>
-            Codeblock
-          </Text>
-        </SidebarButton>
+      <SidebarButton
+        onPress={() => editor?.chain().focus().toggleCodeBlock().run()}
+      >
+        <EditorSidebarIcon
+          isActive={editor?.isActive("codeBlock") || false}
+          name="code-s-slash-line"
+        />
+        <Text variant="xs" bold={editor?.isActive("codeBlock") || false}>
+          Codeblock
+        </Text>
+      </SidebarButton>
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleBlockquote().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("blockquote") || false}
-            name="double-quotes-l"
-          />
-          <Text variant="xs" bold={editor?.isActive("blockquote") || false}>
-            Blockquote
-          </Text>
-        </SidebarButton>
+      <SidebarButton
+        onPress={() => editor?.chain().focus().toggleBlockquote().run()}
+      >
+        <EditorSidebarIcon
+          isActive={editor?.isActive("blockquote") || false}
+          name="double-quotes-l"
+        />
+        <Text variant="xs" bold={editor?.isActive("blockquote") || false}>
+          Blockquote
+        </Text>
+      </SidebarButton>
 
-        <SidebarDivider />
+      <SidebarDivider />
 
-        <Heading lvl={4} style={tw`ml-4`} padded>
-          Lists
-        </Heading>
+      <Heading lvl={4} style={tw`ml-4`} padded>
+        Lists
+      </Heading>
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleBulletList().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("bulletList") || false}
-            name="list-unordered"
-          />
-          <Text variant="xs" bold={editor?.isActive("bulletList") || false}>
-            Bullet-List
-          </Text>
-        </SidebarButton>
+      <SidebarButton
+        onPress={() => editor?.chain().focus().toggleBulletList().run()}
+      >
+        <EditorSidebarIcon
+          isActive={editor?.isActive("bulletList") || false}
+          name="list-unordered"
+        />
+        <Text variant="xs" bold={editor?.isActive("bulletList") || false}>
+          Bullet-List
+        </Text>
+      </SidebarButton>
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleOrderedList().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("orderedList") || false}
-            name="list-ordered"
-          />
-          <Text variant="xs" bold={editor?.isActive("orderedList") || false}>
-            Numbered-List
-          </Text>
-        </SidebarButton>
+      <SidebarButton
+        onPress={() => editor?.chain().focus().toggleOrderedList().run()}
+      >
+        <EditorSidebarIcon
+          isActive={editor?.isActive("orderedList") || false}
+          name="list-ordered"
+        />
+        <Text variant="xs" bold={editor?.isActive("orderedList") || false}>
+          Numbered-List
+        </Text>
+      </SidebarButton>
 
-        <SidebarButton
-          onPress={() => editor?.chain().focus().toggleTaskList().run()}
-        >
-          <EditorSidebarIcon
-            isActive={editor?.isActive("taskList") || false}
-            name="list-check-2"
-          />
-          <Text variant="xs" bold={editor?.isActive("taskList") || false}>
-            Check-List
-          </Text>
-        </SidebarButton>
+      <SidebarButton
+        onPress={() => editor?.chain().focus().toggleTaskList().run()}
+      >
+        <EditorSidebarIcon
+          isActive={editor?.isActive("taskList") || false}
+          name="list-check-2"
+        />
+        <Text variant="xs" bold={editor?.isActive("taskList") || false}>
+          Check-List
+        </Text>
+      </SidebarButton>
 
-        <SidebarDivider />
+      <SidebarDivider />
 
-        <Heading lvl={4} style={tw`ml-4`} padded>
-          Images
-        </Heading>
+      <Heading lvl={4} style={tw`ml-4`} padded>
+        Images
+      </Heading>
 
-        <SidebarButton
-          onPress={() => {
-            initiateImagePicker({
-              encryptAndUploadFile,
-              insertImage: ({ uploadId, width, height }: InsertImageParams) => {
-                if (!editor) {
-                  return;
-                }
-                editor.commands.insertContent({
-                  type: "image",
-                  attrs: {
-                    uploadId,
-                    width,
-                    height,
-                  },
-                });
-              },
-              updateImageAttributes: (params: UpdateImageAttributesParams) => {
-                if (!editor) {
-                  return;
-                }
-                updateImageAttributes({ ...params, view: editor.view });
-              },
-            });
-          }}
-        >
-          <EditorSidebarIcon isActive={false} name="image-line" />
-          <Text variant="xs" bold={false}>
-            Upload Image
-          </Text>
-        </SidebarButton>
-      </div>
+      <SidebarButton
+        onPress={() => {
+          initiateImagePicker({
+            encryptAndUploadFile,
+            insertImage: ({ uploadId, width, height }: InsertImageParams) => {
+              if (!editor) {
+                return;
+              }
+              editor.commands.insertContent({
+                type: "image",
+                attrs: {
+                  uploadId,
+                  width,
+                  height,
+                },
+              });
+            },
+            updateImageAttributes: (params: UpdateImageAttributesParams) => {
+              if (!editor) {
+                return;
+              }
+              updateImageAttributes({ ...params, view: editor.view });
+            },
+          });
+        }}
+      >
+        <EditorSidebarIcon isActive={false} name="image-line" />
+        <Text variant="xs" bold={false}>
+          Upload Image
+        </Text>
+      </SidebarButton>
     </View>
   );
 }
