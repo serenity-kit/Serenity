c1cc15f2f2721f60107d292d0f0142569058ac31
Nik Graf <nik@nikgraf.com>
2022-11-06 10:05:49 +0100

    upgrade native-base

--- add-ts-no-check.js
+++ add-ts-no-check.js
@@ -2,19 +2,7 @@ const fs = require("fs");
 
 const ADDED_STR = "// @ts-nocheck\n\n";
 const FILES = [
-  "../../node_modules/native-base/src/components/composites/IconButton/types.ts",
-  "../../node_modules/native-base/src/components/composites/Popover/PopoverContent.tsx",
-  "../../node_modules/native-base/src/components/composites/SimpleGrid/SimpleGrid.tsx",
-  "../../node_modules/native-base/src/components/composites/Skeleton/SkeletonText.tsx",
-  "../../node_modules/native-base/src/components/composites/Toast/Toast.tsx",
-  "../../node_modules/native-base/src/components/primitives/Icon/types.ts",
-  "../../node_modules/native-base/src/components/primitives/Input/InputGroup.tsx",
-  "../../node_modules/native-base/src/components/primitives/Radio/Radio.tsx",
-  "../../node_modules/native-base/src/components/primitives/Radio/RadioGroup.tsx",
-  "../../node_modules/native-base/src/components/primitives/Slider/Slider.tsx",
-  "../../node_modules/native-base/src/components/primitives/Switch/types.ts",
-  "../../node_modules/native-base/src/hooks/useMediaQuery.ts",
-  "../../node_modules/native-base/src/utils/getSpacedChildren.tsx",
+  // "../../node_modules/native-base/src/utils/getSpacedChildren.tsx",
 ];
 
 Promise.allSettled(FILES.map(addTsNoCheck)).then((results) => {
