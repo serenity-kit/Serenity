db65ff8f7a79ae83954e623559e019042ea727c8
Nik Graf <nik@nikgraf.com>
2022-11-06 13:46:23 +0100

    first draft of a page share link ui (#490)

--- apps/app/components/pageShareModalContent/PageShareModalContent.tsx
+++ apps/app/components/pageShareModalContent/PageShareModalContent.tsx
@@ -1,35 +1,83 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
 import {
   Button,
-  Checkbox,
   IconButton,
   InfoMessage,
-  Input,
   List,
   ListHeader,
   ListItem,
   ListText,
   Spinner,
   Text,
+  Tooltip,
   tw,
   useIsDesktopDevice,
+  View,
 } from "@serenity-tools/ui";
+import * as Clipboard from "expo-clipboard";
 import { useState } from "react";
+import { StyleSheet } from "react-native";
 import { useDocumentShareLinksQuery } from "../../generated/graphql";
+import { useWorkspaceContext } from "../../hooks/useWorkspaceContext";
 import { WorkspaceDrawerParamList } from "../../types/navigation";
+import { createDocumentShareLink } from "../../utils/document/createDocumentShareLink";
 import { notNull } from "../../utils/notNull/notNull";
 
+const styles = StyleSheet.create({
+  createShareLinkButton: tw`mb-8 self-start`,
+});
+
+const CLIPBOARD_NOTICE_TIMEOUT_SECONDS = 2;
+
 export function PageShareModalContent() {
-  const [isLinkSharing, setIsLinkSharing] = useState(false);
   const route = useRoute<RouteProp<WorkspaceDrawerParamList, "Page">>();
   const [documentShareLinksResult] = useDocumentShareLinksQuery({
     variables: { documentId: route.params.pageId },
   });
   const isDesktopDevice = useIsDesktopDevice();
+  const { activeDevice } = useWorkspaceContext();
+
+  const [isClipboardNoticeActive, setIsClipboardNoticeActive] = useState(false);
+  const [selectedPageShareLinkToken, setSelectedPageShareLinkToken] = useState<
+    string | null
+  >(null);
+  const [pageShareLink, setPageShareLink] = useState<string | null>(null);
 
   const documentShareLinks =
     documentShareLinksResult.data?.documentShareLinks?.nodes?.filter(notNull) ||
     [];
+
+  const createShareLink = async () => {
+    if (!activeDevice.encryptionPrivateKey) {
+      console.error("active device doesn't have encryptionPrivateKey");
+      return;
+    }
+    const { encryptionPrivateKey, signingPrivateKey, ...creatorDevice } =
+      activeDevice;
+    try {
+      const shareLinkData = await createDocumentShareLink({
+        documentId: route.params.pageId,
+        creatorDevice,
+        creatorDeviceEncryptionPrivateKey: encryptionPrivateKey,
+      });
+      console.log(shareLinkData);
+      setSelectedPageShareLinkToken(shareLinkData.token);
+      setPageShareLink(shareLinkData.documentShareLink);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const copyInvitationText = () => {
+    if (!pageShareLink) {
+      return;
+    }
+    Clipboard.setString(pageShareLink);
+    setIsClipboardNoticeActive(true);
+    setTimeout(() => {
+      setIsClipboardNoticeActive(false);
+    }, CLIPBOARD_NOTICE_TIMEOUT_SECONDS * 1000);
+  };
 
   return (
     <>
@@ -42,74 +90,82 @@ export function PageShareModalContent() {
               Failed to fetch the page share links. Please try again later.
             </InfoMessage>
           ) : (
-            <List
-              data={documentShareLinks}
-              emptyString={"No active share links"}
-              header={<ListHeader data={["Page Share Links"]} />}
-            >
-              {documentShareLinks.map((documentShareLink) => {
-                return (
-                  <ListItem
-                    key={documentShareLink.token}
-                    // onSelect={() => props.onSelect(documentShareLink.token)}
-                    mainItem={
-                      <>
-                        <ListText style={[tw`w-1/2 md:w-2/3`]}>
-                          https://serenity.re/accept-workspace-invitation
-                        </ListText>
-                        <ListText>/</ListText>
-                        <ListText style={[tw`w-1/2 md:w-1/4`]} bold>
-                          {documentShareLink.token}
-                        </ListText>
-                      </>
-                    }
-                    actionItem={
+            <>
+              <View>
+                <Text
+                  variant="xs"
+                  testID="workspaceInvitationInstructionsText"
+                  selectable={selectedPageShareLinkToken !== null}
+                >
+                  {selectedPageShareLinkToken !== null
+                    ? pageShareLink
+                    : 'The share link will be generated here\nClick on "Create page share link" to generate a new link'}
+                </Text>
+                {selectedPageShareLinkToken !== null ? (
+                  <View style={tw`absolute right-3 top-3`}>
+                    <Tooltip
+                      label={
+                        isClipboardNoticeActive
+                          ? "Copying..."
+                          : "Copy to clipboard"
+                      }
+                      placement={"left"}
+                    >
                       <IconButton
-                        name={"delete-bin-line"}
-                        color={isDesktopDevice ? "gray-900" : "gray-700"}
-                        onPress={() => {
-                          // TODO delete documentShareLink
-                        }}
+                        name="file-copy-line"
+                        color={"primary-300"}
+                        transparent
+                        onPress={copyInvitationText}
+                        isLoading={isClipboardNoticeActive}
                       />
-                    }
-                  />
-                );
-              })}
-            </List>
+                    </Tooltip>
+                  </View>
+                ) : null}
+              </View>
+              <Button
+                onPress={createShareLink}
+                style={styles.createShareLinkButton}
+              >
+                Create Page Link
+              </Button>
+              <List
+                data={documentShareLinks}
+                emptyString={"No active share links"}
+                header={<ListHeader data={["Page Share Links"]} />}
+              >
+                {documentShareLinks.map((documentShareLink) => {
+                  return (
+                    <ListItem
+                      key={documentShareLink.token}
+                      // onSelect={() => props.onSelect(documentShareLink.token)}
+                      mainItem={
+                        <>
+                          <ListText style={[tw`w-1/2 md:w-2/3`]}>
+                            https://serenity.re/accept-workspace-invitation
+                          </ListText>
+                          <ListText>/</ListText>
+                          <ListText style={[tw`w-1/2 md:w-1/4`]} bold>
+                            {documentShareLink.token}
+                          </ListText>
+                        </>
+                      }
+                      actionItem={
+                        <IconButton
+                          name={"delete-bin-line"}
+                          color={isDesktopDevice ? "gray-900" : "gray-700"}
+                          onPress={() => {
+                            // TODO delete documentShareLink
+                          }}
+                        />
+                      }
+                    />
+                  );
+                })}
+              </List>
+            </>
           )}
         </>
       )}
-
-      <Checkbox
-        value={"linkSharing"}
-        isChecked={isLinkSharing}
-        onChange={setIsLinkSharing}
-      >
-        <Text variant="xs" muted>
-          Share via Link
-        </Text>
-      </Checkbox>
-      {isLinkSharing ? (
-        <>
-          <Checkbox value={"canComment"} isChecked={true} isDisabled>
-            <Text variant="xs" muted>
-              Can comment
-            </Text>
-          </Checkbox>
-          <Checkbox value={"canEdit"} isChecked={true} isDisabled>
-            <Text variant="xs" muted>
-              Can edit
-            </Text>
-          </Checkbox>
-          <Text></Text>
-          <Input
-            label={"Link"}
-            value={"https://example.com/dummy/link"}
-            isDisabled
-          />
-          <Button size="sm">Copy</Button>
-        </>
-      ) : null}
     </>
   );
 }
