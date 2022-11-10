d35a0b784a2c75af661792c00d6d236940c5448d
Adonis Gaitatzis <backupbrain@gmail.com>
2022-11-10 15:15:29 +0100

    back-end guards preventing non-admin and non-editor from deleting a document

--- apps/backend/test/helpers/document/deleteDocuments.ts
+++ apps/backend/test/helpers/document/deleteDocuments.ts
@@ -3,31 +3,29 @@ import { gql } from "graphql-request";
 type Params = {
   graphql: any;
   ids: string[];
+  workspaceId: string;
   authorizationHeader: string;
 };
 
 export const deleteDocuments = async ({
   graphql,
   ids,
+  workspaceId,
   authorizationHeader,
 }: Params) => {
   const authorizationHeaders = {
     authorization: authorizationHeader,
   };
   const query = gql`
-        mutation {
-          deleteDocuments(
-            input: {
-              ids: "${ids}"
-            }
-          ) {
-            status
-          }
-        }
-      `;
+    mutation deleteDocuments($input: DeleteDocumentsInput!) {
+      deleteDocuments(input: $input) {
+        status
+      }
+    }
+  `;
   const result = await graphql.client.request(
     query,
-    null,
+    { input: { ids, workspaceId } },
     authorizationHeaders
   );
   return result;
