a3bca576f32a08b21fcbd6904681bd338d1b1982
Adonis Gaitatzis <backupbrain@gmail.com>
2022-11-11 10:57:52 +0100

    restricting file upload and download to document owners

--- apps/backend/src/graphql/mutations/file/initiateFileUpload.ts
+++ apps/backend/src/graphql/mutations/file/initiateFileUpload.ts
@@ -1,6 +1,4 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 // @ts-ignore
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { AuthenticationError } from "apollo-server-express";
 import {
   arg,
@@ -9,26 +7,7 @@ import {
   nonNull,
   objectType,
 } from "nexus";
-import { v4 as uuidv4 } from "uuid";
-
-if (
-  !process.env.FILE_STORAGE_ACCOUNT_ID ||
-  !process.env.FILE_STORAGE_ACCESS_KEY_ID ||
-  !process.env.FILE_STORAGE_SECRET_ACCESS_KEY ||
-  !process.env.FILE_STORAGE_BUCKET
-) {
-  throw new Error("Missing environment variables for file storage");
-}
-
-const bucketUrl = `https://${process.env.FILE_STORAGE_ACCOUNT_ID}.r2.cloudflarestorage.com`;
-const s3Client = new S3Client({
-  region: "auto",
-  endpoint: bucketUrl,
-  credentials: {
-    accessKeyId: process.env.FILE_STORAGE_ACCESS_KEY_ID,
-    secretAccessKey: process.env.FILE_STORAGE_SECRET_ACCESS_KEY,
-  },
-});
+import { initiateFileUpload } from "../../../database/file/initiateFileUpload";
 
 export const InitiateFileUploadInput = inputObjectType({
   name: "InitiateFileUploadInput",
@@ -59,19 +38,11 @@ export const initiateFileUploadMutation = mutationField("initiateFileUpload", {
     if (!context.user) {
       throw new AuthenticationError("Not authenticated");
     }
-
-    const fileId = uuidv4();
-    const fileName = `${args.input.workspaceId}/${args.input.documentId}/${fileId}.blob`;
-    const uploadUrl = await getSignedUrl(
-      s3Client,
-      new PutObjectCommand({
-        Bucket: process.env.FILE_STORAGE_BUCKET,
-        Key: fileName,
-        ContentType: "application/octet-stream",
-      }),
-      { expiresIn: 60 } // seconds
-    );
-
-    return { uploadUrl, fileId };
+    const response = await initiateFileUpload({
+      userId: context.user.id,
+      documentId: args.input.documentId,
+      workspaceId: args.input.workspaceId,
+    });
+    return response;
   },
 });
---
+++ apps/backend/src/graphql/queries/file/fileUrl.test.ts
@@ -0,0 +1,178 @@
+import { gql } from "graphql-request";
+import { v4 as uuidv4 } from "uuid";
+import { registerUser } from "../../../../test/helpers/authentication/registerUser";
+import deleteAllRecords from "../../../../test/helpers/deleteAllRecords";
+import { initateFileUpload } from "../../../../test/helpers/file/initiateFileUpload";
+import setupGraphql, {
+  TestContext,
+} from "../../../../test/helpers/setupGraphql";
+import createUserWithWorkspace from "../../../database/testHelpers/createUserWithWorkspace";
+
+const graphql = setupGraphql();
+const password = "password";
+let userData: any = undefined;
+let fileUploadData: any = undefined;
+
+const setup = async () => {
+  userData = await createUserWithWorkspace({
+    id: uuidv4(),
+    username: `${uuidv4()}@example.com`,
+    password,
+  });
+  const fileUploadResult = await initateFileUpload({
+    graphql,
+    documentId: userData.document.id,
+    workspaceId: userData.workspace.id,
+    authorization: userData.sessionKey,
+  });
+  fileUploadData = fileUploadResult.initiateFileUpload;
+};
+
+beforeAll(async () => {
+  await deleteAllRecords();
+  await setup();
+});
+
+type Props = {
+  graphql: TestContext;
+  fileId: string;
+  workspaceId: string;
+  documentId: string;
+  authorization: string;
+};
+const getFileUrl = async ({
+  graphql,
+  fileId,
+  documentId,
+  workspaceId,
+  authorization,
+}: Props) => {
+  const query = gql`
+    query fileUrl($fileId: ID!, $documentId: ID!, $workspaceId: ID!) {
+      fileUrl(
+        fileId: $fileId
+        documentId: $documentId
+        workspaceId: $workspaceId
+      ) {
+        id
+        downloadUrl
+      }
+    }
+  `;
+  return graphql.client.request(
+    query,
+    { fileId, documentId, workspaceId },
+    { authorization }
+  );
+};
+
+test("get file url", async () => {
+  const result = await getFileUrl({
+    graphql,
+    fileId: fileUploadData.fileId,
+    documentId: userData.document.id,
+    workspaceId: userData.workspace.id,
+    authorization: userData.sessionKey,
+  });
+  const fileUrl = result.fileUrl;
+  expect(typeof fileUrl.id).toBe("string");
+  expect(typeof fileUrl.downloadUrl).toBe("string");
+  expect(fileUrl.downloadUrl).toContain("https://");
+});
+
+test("invalid access", async () => {
+  const otherUser = await registerUser(
+    graphql,
+    `${uuidv4()}@example.com`,
+    "password"
+  );
+  await expect(
+    (async () =>
+      await getFileUrl({
+        graphql,
+        fileId: fileUploadData.fileId,
+        documentId: userData.document.id,
+        workspaceId: userData.workspace.id,
+        authorization: otherUser.sessionKey,
+      }))()
+  ).rejects.toThrow("Unauthorized");
+});
+
+test("Unauthenticated", async () => {
+  await expect(
+    (async () =>
+      await getFileUrl({
+        graphql,
+        fileId: fileUploadData.fileId,
+        documentId: userData.document.id,
+        workspaceId: userData.workspace.id,
+        authorization: "invalid-session-key",
+      }))()
+  ).rejects.toThrowError(/UNAUTHENTICATED/);
+});
+
+describe("Input errors", () => {
+  const query = gql`
+    mutation initiateFileUpload($input: InitiateFileUploadInput!) {
+      initiateFileUpload(input: $input) {
+        fileId
+        uploadUrl
+      }
+    }
+  `;
+  const id = uuidv4();
+  test("Invalid file id", async () => {
+    const userData = await createUserWithWorkspace({
+      id: uuidv4(),
+      username: `${uuidv4()}@example.com`,
+      password,
+    });
+    await expect(
+      (async () =>
+        await graphql.client.request(
+          query,
+          {
+            input: {
+              fileId: null,
+              documentId: userData.document.id,
+              workspaceId: userData.workspace.id,
+            },
+          },
+          { authorization: userData.sessionKey }
+        ))()
+    ).rejects.toThrowError();
+  });
+  test("Invalid document id", async () => {
+    const userData = await createUserWithWorkspace({
+      id: uuidv4(),
+      username: `${uuidv4()}@example.com`,
+      password,
+    });
+    await expect(
+      (async () =>
+        await graphql.client.request(
+          query,
+          {
+            input: {
+              documentId: userData.document.id,
+              workspaceId: null,
+            },
+          },
+          { authorization: userData.sessionKey }
+        ))()
+    ).rejects.toThrowError();
+  });
+  test("Invalid input", async () => {
+    const userData = await createUserWithWorkspace({
+      id: uuidv4(),
+      username: `${uuidv4()}@example.com`,
+      password,
+    });
+    await expect(
+      (async () =>
+        await graphql.client.request(query, null, {
+          authorization: userData.sessionKey,
+        }))()
+    ).rejects.toThrowError();
+  });
+});
