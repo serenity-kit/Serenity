ebbb9f8bdd3141072b9fc75cb439853c9a29b258
Nik Graf <nik@nikgraf.com>
2022-11-11 23:10:16 +0100

    add empty share page

---
+++ apps/app/navigation/screens/sharePageScreen/SharePageScreen.tsx
@@ -0,0 +1,8 @@
+import { Heading } from "@serenity-tools/ui";
+import { RootStackScreenProps } from "../../../types/navigation";
+
+export default function SharePageScreen(
+  props: RootStackScreenProps<"SharePage">
+) {
+  return <Heading lvl={1}>To be implemented</Heading>;
+}
