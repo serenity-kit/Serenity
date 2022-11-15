761efda1feabddc8d6bc8585bd042c30d55a8641
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-11-15 17:13:18 +0100

    Add status to Avatar for collaboration usage

--- packages/ui/components/avatar/Avatar.tsx
+++ packages/ui/components/avatar/Avatar.tsx
@@ -3,28 +3,56 @@ import { StyleSheet } from "react-native";
 import { Avatar as NbAvatar, IAvatarProps } from "native-base";
 import { tw } from "../../tailwind";
 import { CollaborationColor } from "../../types";
+import { View } from "../view/View";
 
 export type AvatarProps = IAvatarProps & {
   customColor?: CollaborationColor;
+  size?: "2xl" | "xl" | "lg" | "md" | "sm" | "xs" | "xxs";
+  status?: "active" | "inactive" | "default";
 };
 
 export const Avatar = forwardRef((props: AvatarProps, ref) => {
-  const { size = "sm", customColor = "serenity" } = props;
+  const { size = "sm", customColor = "serenity", status = "default" } = props;
   const styles = StyleSheet.create({
     avatar: tw``,
   });
+
+  const statusStyling = {
+    active: tw`border-2 border-white`,
+    inactive: tw`opacity-50`,
+    default: tw``,
+  };
+
+  const activeIndicatorSize = {
+    "2xl": 33,
+    xl: 25,
+    lg: 17,
+    md: 13,
+    sm: 9,
+    xs: 7,
+    xxs: 6,
+  };
 
   return (
     <NbAvatar
       {...props}
       size={size}
-      style={[styles.avatar, props.style]}
+      style={[styles.avatar, statusStyling[status], props.style]}
       _text={{
         fontFamily: "Inter_600SemiBold",
         style: tw`uppercase`,
       }}
       bg={`collaboration.${customColor}`}
     >
+      {status === "active" ? (
+        <View
+          style={[
+            tw`absolute w-${activeIndicatorSize[size]} h-${activeIndicatorSize[size]} bg-transparent`,
+            tw`rounded-full border-2 border-collaboration-${customColor}`,
+            { zIndex: -1 },
+          ]}
+        />
+      ) : null}
       {props.children}
     </NbAvatar>
   );
