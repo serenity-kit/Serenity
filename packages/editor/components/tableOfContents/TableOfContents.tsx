15cd03dd431238290be7e4560d11e85eed034bc3
Nik Graf <nik@nikgraf.com>
2022-11-18 09:01:52 +0100

    add table of contents

--- packages/editor/components/tableOfContents/TableOfContents.tsx
+++ packages/editor/components/tableOfContents/TableOfContents.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "@serenity-tools/ui";
+import { Text, tw, View } from "@serenity-tools/ui";
 import { Editor } from "@tiptap/react";
 import React from "react";
 
@@ -7,9 +7,29 @@ type Props = {
 };
 
 export default function TableOfContents({ editor }: Props) {
+  const content = editor?.getJSON().content || [];
+
   return (
-    <View>
-      <Text>Table of Contents</Text>
+    <View style={tw`pl-2`}>
+      {content.map((entry, index) => {
+        if (entry.type !== "heading") {
+          return null;
+        }
+        return (
+          <Text
+            key={index}
+            variant="sm"
+            style={tw`pl-${entry.attrs?.level * 2 || 0}`}
+          >
+            {entry.content?.map((subEntry, index) => {
+              if (subEntry.type !== "text") {
+                return null;
+              }
+              return <>{subEntry.text}</>;
+            })}
+          </Text>
+        );
+      })}
     </View>
   );
 }
