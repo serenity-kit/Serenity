cd4cfe40c9a47cfa5ead37ac9420c0e9678974c2
Nik Graf <nik@nikgraf.com>
2022-11-18 22:17:44 +0100

    improve share page screen

--- apps/app/navigation/screens/sharePageScreen/SharePageScreen.tsx
+++ apps/app/navigation/screens/sharePageScreen/SharePageScreen.tsx
@@ -1,7 +1,8 @@
 import { useFocusEffect } from "@react-navigation/native";
-import { Heading } from "@serenity-tools/ui";
+import sodium, { KeyPair } from "@serenity-tools/libsodium";
+import { CenterContent, InfoMessage, Spinner } from "@serenity-tools/ui";
 import { useMachine } from "@xstate/react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { RootStackScreenProps } from "../../../types/navigationProps";
 import { sharePageScreenMachine } from "./sharePageScreenMachine";
 
@@ -22,13 +23,45 @@ export default function SharePageScreen(
     send("start");
   });
 
-  if (state.value !== "done" && state.value !== "decryptDeviceFail") {
-    return <Heading lvl={1}>loading...</Heading>;
-  } else if (state.value === "decryptDeviceFail") {
-    return <Heading lvl={1}>decryption failed</Heading>;
-  } else {
-    return <Heading lvl={1}>done</Heading>;
-  }
+  const signatureKeyPair: KeyPair | null = useMemo(() => {
+    if (state.context.device) {
+      return {
+        publicKey: sodium.from_base64(state.context.device.signingPublicKey),
+        privateKey: sodium.from_base64(state.context.device.signingPrivateKey!),
+        keyType: "ed25519",
+      };
+    }
+    return null;
+  }, [state.context.device]);
 
-  // return <Heading lvl={1}>To be implemented</Heading>;
+  if (state.value !== "done" && state.value !== "decryptDeviceFail") {
+    return (
+      <CenterContent>
+        <Spinner fadeIn />
+      </CenterContent>
+    );
+  } else if (state.value === "decryptDeviceFail") {
+    return (
+      <CenterContent>
+        <InfoMessage variant="error">
+          Failed decrypting document access. Please ask for a new share link.
+        </InfoMessage>
+      </CenterContent>
+    );
+  } else if (signatureKeyPair) {
+    return (
+      <CenterContent>
+        <InfoMessage>Show Page</InfoMessage>
+      </CenterContent>
+      // <Page
+      //   navigation={props.navigation}
+      //   route={props.route}
+      //   // to force unmount and mount the page
+      //   key={state.context.documentId}
+      //   updateTitle={() => {}}
+      //   signatureKeyPair={signatureKeyPair}
+      //   workspaceId={"TODO"}
+      // />
+    );
+  }
 }
