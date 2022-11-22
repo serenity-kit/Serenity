9690e3320590b28884a77936900154e271fa2232
Susanne Kristufek <s.kristufek@eventbutler.io>
2022-11-22 12:11:50 +0100

    Add shimmer to Image extension

--- packages/editor-image-extension/src/Image.tsx
+++ packages/editor-image-extension/src/Image.tsx
@@ -93,37 +93,39 @@ export const Image = (props: any) => {
       {state.step !== "done" ? (
         <View
           style={[
-            tw`flex justify-center items-center bg-gray-200`,
+            tw``,
             {
               aspectRatio: `1 / ${height / width}`,
             },
           ]}
         >
-          <View
-            style={tw`max-${isPortrait ? "w" : "h"}-30 ${
-              isPortrait ? "w-1/3" : "h-2/5"
-            }`}
-          >
-            <Icon
-              name="image-2-line"
-              color={"gray-300"}
-              size="full"
-              mobileSize={"full"}
-            />
-          </View>
-          <Text
-            variant={isDesktopDevice || isPortrait ? "xs" : "xxs"}
-            style={tw`text-gray-400 opacity-80`}
-            bold
-          >
-            {
+          <div className="shimmerBG flex h-full w-full flex-col items-center justify-center">
+            <View
+              style={tw`max-${isPortrait ? "w" : "h"}-30 ${
+                isPortrait ? "w-1/3" : "h-2/5"
+              }`}
+            >
+              <Icon
+                name="image-2-line"
+                color={"gray-300"}
+                size="full"
+                mobileSize={"full"}
+              />
+            </View>
+            <Text
+              variant={isDesktopDevice || isPortrait ? "xs" : "xxs"}
+              style={tw`text-gray-400 opacity-80`}
+              bold
+            >
               {
-                downloading: "Downloading …",
-                uploading: "Uploading …",
-                failedToDecrypt: "Failed to decrypt",
-              }[state.step]
-            }
-          </Text>
+                {
+                  downloading: "Downloading …",
+                  uploading: "Uploading …",
+                  failedToDecrypt: "Failed to decrypt",
+                }[state.step]
+              }
+            </Text>
+          </div>
         </View>
       ) : (
         <img src={state.contentAsBase64!} />
