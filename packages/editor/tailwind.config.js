48b5206b8a567c18942e584ec1fb928dde288276
Nik Graf <nik@nikgraf.com>
2022-12-23 14:43:16 +0100

    include other packages

--- packages/editor/tailwind.config.js
+++ packages/editor/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./**/*.{tsx,html}"],
+  content: [
+    "./**/*.{tsx,html}",
+    "../editor*/**/*.{html,tsx}", // also include other editor-packages like the editor-file-extension
+  ],
   presets: [require("../../tailwind.config")],
   plugins: [require("@tailwindcss/typography")],
 };
