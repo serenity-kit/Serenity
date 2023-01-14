1d591b65f0444a2404c24e3b0878cff2d017f1d8
Nik Graf <nik@nikgraf.com>
2023-01-14 11:52:20 +0100

    add back sqlite3

--- apps/desktop-app/forge.config.js
+++ apps/desktop-app/forge.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 module.exports = {
   name: "Serenity",
@@ -16,6 +17,23 @@ module.exports = {
       appleId: process.env.APPLE_ID,
       appleIdPassword: process.env.APPLE_ID_PASSWORD,
       teamId: process.env.APPLE_TEAM_ID,
+    },
+  },
+  rebuildConfig: {},
+  hooks: {
+    packageAfterPrune: async (forgeConfig, buildPath) => {
+      const sqliteBuildPath = path.join(
+        buildPath,
+        "node_modules",
+        "sqlite3",
+        "build"
+      );
+      console.log("Sqlite BuildPath: ", sqliteBuildPath);
+      // needs to be deleted otherwise macos codesign will fail
+      fs.rmSync(sqliteBuildPath, {
+        recursive: true,
+        force: true,
+      });
     },
   },
   makers: [
