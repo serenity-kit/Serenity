371c38d08fda6d7ff2fc2dc4362f7fe1495a1bb8
Nik Graf <nik@nikgraf.com>
2023-01-15 09:18:15 +0100

    setup macos signing certs (#587)
    * setup macos signing certs
    
    * add signing
    
    * fix macos signing timeout
    
    * add notarizing
    
    * reduce upload artifacts

--- apps/desktop-app/forge.config.js
+++ apps/desktop-app/forge.config.js
@@ -1,10 +1,41 @@
 const path = require("path");
+const fs = require("fs");
 
 module.exports = {
+  name: "Serenity",
+  appBundleId: "re.serenity.desktop-app",
+  // asar: true,
   packagerConfig: {
+    executableName: "serenity-desktop-app",
     icon: path.resolve(__dirname, "icons", "icon"),
+    osxSign: {
+      // "hardened-runtime": true,
+      // "gatekeeper-assess": false,
+    },
+    osxNotarize: {
+      tool: "notarytool",
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    },
   },
   rebuildConfig: {},
+  hooks: {
+    packageAfterPrune: async (forgeConfig, buildPath) => {
+      const sqliteBuildPath = path.join(
+        buildPath,
+        "node_modules",
+        "sqlite3",
+        "build"
+      );
+      // console.log("Sqlite BuildPath: ", sqliteBuildPath);
+      // needs to be deleted otherwise macos codesign will fail
+      fs.rmSync(sqliteBuildPath, {
+        recursive: true,
+        force: true,
+      });
+    },
+  },
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
