2fde92e49424939a1d1eae91b5bc0199afa5e37f
Nik Graf <nik@nikgraf.com>
2023-01-20 20:53:51 +0100

    fix verifyDevice test

--- packages/common/src/verifyDevice/verifyDevice.test.ts
+++ packages/common/src/verifyDevice/verifyDevice.test.ts
@@ -8,26 +8,26 @@ beforeAll(async () => {
 
 test("verify device", () => {
   const device = createDevice();
-  expect(verifyDevice(device)).resolves.toBeUndefined();
+  expect(verifyDevice(device)).toBeUndefined();
 });
 
 test("verify device throws an error with invalid signature", () => {
   const device = createDevice();
-  expect(
+  expect(() =>
     verifyDevice({
       ...device,
       encryptionPublicKeySignature: "invalid",
     })
-  ).rejects.toThrowError();
+  ).toThrowError();
 });
 
 test("verify device throws an error with ommited signature", () => {
   const device = createDevice();
-  expect(
+  expect(() =>
     verifyDevice({
       ...device,
       // @ts-expect-error desired for the test
       encryptionPublicKeySignature: undefined,
     })
-  ).rejects.toThrowError();
+  ).toThrowError();
 });
