f92e87d1f5b933567828b32a74946409a6b0fa59
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-01-31 16:35:17 +0100

    Refactor RawInput to extract custom stylings

--- packages/ui/components/rawInput/RawInput.tsx
+++ packages/ui/components/rawInput/RawInput.tsx
@@ -14,77 +14,66 @@ export const RawInput = forwardRef((props: RawInputProps, ref) => {
   const isEqualOrLargerThanXS = useIsEqualOrLargerThanBreakpoint("xs");
   const { size = isEqualOrLargerThanXS ? "md" : "lg" } = props;
 
-  const styles = StyleSheet.create({
-    wrapper: tw`rounded ${size == "md" ? "h-10" : "h-form-element"}`,
-    input: tw`px-4 flex justify-center font-input text-gray-900 ${
-      size == "md" ? "text-xs" : "text-input"
-    }`,
-  });
-
   return (
     <NbInput
       // @ts-ignore
       ref={ref}
       {...props}
-      style={[styles.input, props.isDisabled && tw`text-muted`, props.style]}
+      paddingX={4}
+      display={"flex"}
+      justifyContent="center"
+      fontFamily={"Inter_400Regular"}
+      color={tw.color("gray-900")}
+      style={[tw`${size == "md" ? "text-xs" : "text-input"}`, props.style]}
+      _disabled={{
+        opacity: 0.5,
+        color: tw.color("gray-600"),
+        _stack: {
+          backgroundColor: tw.color("gray-100"),
+        },
+      }}
       _stack={{
-        style: props.isDisabled
-          ? [styles.wrapper, tw`bg-gray-100 border-gray-400`]
-          : [styles.wrapper, tw`bg-white border-gray-400`],
+        height: size === "md" ? 10 : 12,
+        backgroundColor: "white",
+        style: [tw`border-gray-400`],
       }}
       _hover={{
         _stack: {
-          style: props.isDisabled
-            ? [styles.wrapper, tw`bg-gray-100 border-gray-400`]
-            : [styles.wrapper, tw`bg-white border-gray-600`],
+          style: [tw`border-gray-600`],
+        },
+        _disabled: {
+          _stack: {
+            style: [tw`border-gray-400`],
+          },
         },
       }}
       _focus={{
-        _web: {
-          style: [
-            styles.input,
-            props.isDisabled && tw`text-muted`,
-            { outlineStyle: "none" },
-            props.style,
-          ],
-        },
         _stack: {
-          style: [
-            styles.wrapper,
-            tw`bg-white border-primary-400 se-outline-focus-input`,
-          ],
+          // background needs to be set here (nb-override)
+          style: [tw`bg-white border-primary-400 se-outline-focus-input`],
         },
         _hover: {
           _stack: {
-            style: [
-              styles.wrapper,
-              tw`bg-white border-primary-400 se-outline-focus-input`,
-            ],
+            style: [tw`border-primary-400 se-outline-focus-input`],
           },
         },
       }}
       _invalid={{
         _stack: {
-          style: [styles.wrapper, tw`bg-white border-error-500`],
+          style: [tw`border-error-500`],
         },
         _hover: {
           _stack: {
-            style: [styles.wrapper, tw`bg-white border-error-500`],
+            style: [tw`border-error-500`],
           },
         },
         _focus: {
           _stack: {
-            style: [
-              styles.wrapper,
-              tw`bg-white border-error-500 se-outline-error-input`,
-            ],
+            style: [tw`border-error-500 se-outline-error-input`],
           },
           _hover: {
             _stack: {
-              style: [
-                styles.wrapper,
-                tw`bg-white border-error-500 se-outline-error-input`,
-              ],
+              style: [tw`border-error-500 se-outline-error-input`],
             },
           },
         },
