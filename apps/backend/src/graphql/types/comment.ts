c28dba4adcf3b0fe50d96f615ed7359cb3acfee7
Nik Graf <nik@nikgraf.com>
2023-02-16 17:25:13 +0100

    rework create comment mutation

--- apps/backend/src/graphql/types/comment.ts
+++ apps/backend/src/graphql/types/comment.ts
@@ -1,20 +1,18 @@
 import { nonNull, objectType } from "nexus";
 import { CommentReply } from "./commentReply";
 import { CreatorDevice } from "./device";
-import { KeyDerivationTrace2 } from "./keyDerivation";
-import { WorkspaceKey } from "./workspace";
 
 export const Comment = objectType({
   name: "Comment",
   definition(t) {
     t.nonNull.string("id");
     t.nonNull.string("documentId");
+    t.nonNull.string("snapshotId");
+    t.nonNull.int("subkeyId");
     t.nonNull.string("contentCiphertext");
     t.nonNull.string("contentNonce");
     t.nonNull.field("createdAt", { type: nonNull("Date") });
-    t.nonNull.field("keyDerivationTrace", { type: KeyDerivationTrace2 });
     t.nonNull.field("creatorDevice", { type: CreatorDevice });
     t.list.field("commentReplies", { type: CommentReply });
-    t.field("workspaceKey", { type: WorkspaceKey });
   },
 });
