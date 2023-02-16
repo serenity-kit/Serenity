41b1709b1a50c4506f61f88d2facccfbeac825ec
Adonis Gaitatzis <backupbrain@gmail.com>
2023-02-16 16:46:42 +0100

    adding comment e2e tests

--- apps/backend/test/e2e/comment/comment.e2e.ts
+++ apps/backend/test/e2e/comment/comment.e2e.ts
@@ -1,11 +1,9 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 import sodium from "react-native-libsodium";
 import { v4 as uuidv4 } from "uuid";
 import createUserWithWorkspace from "../../../src/database/testHelpers/createUserWithWorkspace";
 import { delayForSeconds } from "../../helpers/delayForSeconds";
 import { login } from "../../helpers/e2e/login";
-import { logout } from "../../helpers/e2e/logout";
-import { reloadPage } from "../../helpers/e2e/reloadPage";
 
 type UserData = {
   id: string;
@@ -37,31 +35,50 @@ test.beforeAll(async () => {
 
 test.describe("Edit document", () => {
   test("Add content", async ({ page }) => {
-    const newContent = "\nHello World!";
+    await page.goto("http://localhost:19006/register");
+    // const { user } = await e2eRegisterUser({
+    //   page,
+    //   username: user1.username,
+    //   password: user1.password,
+    //   workspaceName: "workspace",
+    // });
     await login({
       page,
       username: user1.username,
       password: user1.password,
       stayLoggedIn: true,
     });
-    const editor = page.locator("div[class='ProseMirror']");
-    const startingContent = await editor.innerHTML();
-    await page.type("div[class='ProseMirror']", newContent);
-    await delayForSeconds(2);
-    await reloadPage({ page });
-    await delayForSeconds(2);
-    const endingContent = await editor.innerHTML();
-    expect(startingContent).not.toBe(endingContent);
-    await logout({ page });
-    await login({
-      page,
-      username: user1.username,
-      password: user1.password,
-      stayLoggedIn: true,
+    const header = page.locator("div[class='ProseMirror']");
+
+    // const header = editor.locator(".//h1");
+    // select text inside the editor
+    header.evaluate((element) => {
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.selectNodeContents(element);
+      selection?.removeAllRanges();
+      selection?.addRange(range);
     });
-    await delayForSeconds(2); // wait a bit until the editor loads
-    const editorAfterLogin = page.locator("div[class='ProseMirror']");
-    const afterLoginContent = await editorAfterLogin.innerHTML();
-    expect(afterLoginContent).toBe(endingContent);
+
+    await delayForSeconds(20);
+
+    // const startingContent = await editor.innerHTML();
+    // await page.type("div[class='ProseMirror']", newContent);
+    // await delayForSeconds(2);
+    // await reloadPage({ page });
+    // await delayForSeconds(2);
+    // const endingContent = await editor.innerHTML();
+    // expect(startingContent).not.toBe(endingContent);
+    // await logout({ page });
+    // await login({
+    //   page,
+    //   username: user1.username,
+    //   password: user1.password,
+    //   stayLoggedIn: true,
+    // });
+    // await delayForSeconds(2); // wait a bit until the editor loads
+    // const editorAfterLogin = page.locator("div[class='ProseMirror']");
+    // const afterLoginContent = await editorAfterLogin.innerHTML();
+    // expect(afterLoginContent).toBe(endingContent);
   });
 });
