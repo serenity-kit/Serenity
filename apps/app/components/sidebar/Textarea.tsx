4a204782589716b104de0d74640900742a966c4e
Nik Graf <nik@nikgraf.com>
2023-03-08 13:30:08 +0100

    use rawInput for autogrowing textarea

--- apps/app/components/sidebar/Textarea.tsx
+++ apps/app/components/sidebar/Textarea.tsx
@@ -1,5 +1,8 @@
+import { RawInput } from "@serenity-tools/ui";
 import { useLayoutEffect, useRef } from "react";
-import { Platform, TextInput } from "react-native";
+import { Platform } from "react-native";
+
+const twoRowsHeight = 58;
 
 export const Textarea = (props) => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
@@ -8,18 +11,18 @@ export const Textarea = (props) => {
     if (Platform.OS === "web" && textareaRef.current) {
       // reset the height to get the correct scrollHeight for the textarea
       textareaRef.current.style.height = "0px";
-
-      const computedStyles = window.getComputedStyle(textareaRef.current);
-      const height =
-        parseInt(computedStyles.getPropertyValue("padding-top"), 10) +
-        parseInt(computedStyles.getPropertyValue("padding-bottom"), 10) +
-        parseInt(computedStyles.getPropertyValue("border-top-width"), 10) +
-        parseInt(computedStyles.getPropertyValue("border-bottom-width"), 10) +
-        textareaRef.current.scrollHeight;
-
-      textareaRef.current.style.height = `${height}px`;
+      const height = textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = `${Math.max(height, twoRowsHeight)}px`;
     }
   }, [props.value]);
 
-  return <TextInput {...props} multiline ref={textareaRef} />;
+  return (
+    <RawInput
+      minHeight={twoRowsHeight}
+      height="auto"
+      {...props}
+      multiline
+      ref={textareaRef}
+    />
+  );
 };
