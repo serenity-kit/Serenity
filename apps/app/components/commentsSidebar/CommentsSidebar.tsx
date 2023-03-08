6882088c84f2b27b18dd4a790aba5d24173d0956
Nik Graf <nik@nikgraf.com>
2023-03-08 23:32:50 +0100

    switch to flatlist to make it work on ios

--- apps/app/components/commentsSidebar/CommentsSidebar.tsx
+++ apps/app/components/commentsSidebar/CommentsSidebar.tsx
@@ -2,7 +2,6 @@ import {
   EditorBottombarDivider,
   EditorSidebarHeader,
   Icon,
-  ScrollView,
   Text,
   tw,
   View,
@@ -10,53 +9,66 @@ import {
 import { useActor } from "@xstate/react";
 import { HStack } from "native-base";
 import React, { useRef } from "react";
-import { ScrollView as RNScrollView, StyleSheet } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import { usePage } from "../../context/PageContext";
 import { useMeQuery } from "../../generated/graphql";
 import Comment from "../comment/Comment";
 
-function takeUntil<T>(array: T[], predicate: (value: T) => boolean): T[] {
-  const result: T[] = [];
+const styles = StyleSheet.create({
+  header: tw`justify-between`,
+  wrapper: tw`p-4 border-b border-gray-200`,
+});
 
-  for (const value of array) {
-    if (predicate(value)) {
-      return result;
-    }
-    result.push(value);
-  }
+const Header: React.FC = () => {
+  return (
+    <EditorSidebarHeader style={styles.header}>
+      <Text variant="xs" bold>
+        Comments
+      </Text>
+      <HStack alignItems={"center"} style={tw`-mr-1`}>
+        <Text variant="xxs" muted style={tw`p-1`}>
+          Open
+        </Text>
+        <EditorBottombarDivider style={tw`h-4 border-r border-gray-600`} />
+        <Text variant="xxs" muted style={tw`p-1`}>
+          Resolved
+        </Text>
+      </HStack>
+    </EditorSidebarHeader>
+  );
+};
 
-  return result;
-}
+const EmptyState: React.FC = () => {
+  return (
+    <HStack space={3} style={tw`p-4`}>
+      <View style={tw``}>
+        <Icon name="chat-1-line-message" color={"gray-500"} size={5} />
+      </View>
+      <Text variant="xs" muted>
+        Add suggestions, questions or appreciations by marking a text-passage or
+        image and then clicking on the comment icon in the floating menu.
+      </Text>
+    </HStack>
+  );
+};
 
 const CommentsSidebar: React.FC = () => {
   const { commentsService } = usePage();
   const [meResult] = useMeQuery();
   const [state] = useActor(commentsService);
-
-  const scrollViewRef = useRef<RNScrollView>(null);
-  const commentScrollHeightsRef = useRef<{ id: string; height: number }[]>([]);
-
-  const scrollToCommentId = (commentId: string, animated: boolean) => {
-    const scrollTop = takeUntil(
-      commentScrollHeightsRef.current,
-      (item) => item.id === commentId
-    ).reduce((acc, item) => acc + item.height, 0);
-    if (scrollViewRef.current) {
-      scrollViewRef.current.scrollTo({
-        // the 50 is the fixed height header in the scrollview
-        y: scrollTop + 50,
-        animated,
-      });
-    }
-  };
+  const comments = state.context.decryptedComments;
+  const flatListRef = useRef<FlatList>(null);
 
   commentsService.onTransition((state) => {
-    if (state.context.isOpenSidebar && scrollViewRef.current) {
+    if (state.context.isOpenSidebar && flatListRef.current) {
       if (
         state.event.type === "HIGHLIGHT_COMMENT" &&
         state.context.highlightedCommentId
       ) {
-        scrollToCommentId(state.context.highlightedCommentId, true);
+        const comment = comments.find(
+          (comment) => comment.id === state.context.highlightedCommentId
+        );
+        flatListRef.current?.scrollToItem({ item: comment, animated: true });
       } else if (
         state.event.type === "OPEN_SIDEBAR" ||
         state.event.type === "TOGGLE_SIDEBAR"
@@ -64,12 +76,12 @@ const CommentsSidebar: React.FC = () => {
         // should alays immediately scroll to top once the scrollbar opens
         // and now highlighted comment is set
         if (state.context.highlightedCommentId) {
-          scrollToCommentId(state.context.highlightedCommentId, false);
+          const comment = comments.find(
+            (comment) => comment.id === state.context.highlightedCommentId
+          );
+          flatListRef.current?.scrollToItem({ item: comment, animated: false });
         } else {
-          scrollViewRef.current.scrollTo({
-            y: 0,
-            animated: false,
-          });
+          flatListRef.current.scrollToOffset({ offset: 0, animated: false });
         }
       }
     }
@@ -78,68 +90,19 @@ const CommentsSidebar: React.FC = () => {
   const me = meResult.data?.me;
   if (!me) return null;
 
-  const comments = state.context.decryptedComments;
-
-  const styles = StyleSheet.create({
-    header: tw`justify-between`,
-    wrapper: tw`p-4 border-b border-gray-200`,
-  });
-
   return (
-    // grow-0 overrides default of ScrollView to keep the assigned width
-    <ScrollView style={tw`w-sidebar grow-0 bg-gray-100`} ref={scrollViewRef}>
-      <EditorSidebarHeader style={styles.header}>
-        <Text variant="xs" bold>
-          Comments
-        </Text>
-        <HStack alignItems={"center"} style={tw`-mr-1`}>
-          <Text variant="xxs" muted style={tw`p-1`}>
-            Open
-          </Text>
-          <EditorBottombarDivider style={tw`h-4 border-r border-gray-600`} />
-          <Text variant="xxs" muted style={tw`p-1`}>
-            Resolved
-          </Text>
-        </HStack>
-      </EditorSidebarHeader>
-
-      {comments.length >= 1 ? (
-        <View>
-          {comments.map((comment) => {
-            if (!comment) return null;
-
-            return (
-              <View
-                key={comment.id}
-                onLayout={(event) => {
-                  commentScrollHeightsRef.current.push({
-                    id: comment.id,
-                    height: event.nativeEvent.layout.height,
-                  });
-                  console.log(
-                    "commentScrollHeightsRef.current",
-                    commentScrollHeightsRef.current
-                  );
-                }}
-              >
-                <Comment comment={comment} meId={me.id} meName={me.username} />
-              </View>
-            );
-          })}
-        </View>
-      ) : (
-        <HStack space={3} style={tw`p-4`}>
-          <View style={tw``}>
-            <Icon name="chat-1-line-message" color={"gray-500"} size={5} />
-          </View>
-          <Text variant="xs" muted>
-            Add suggestions, questions or appreciations by marking a
-            text-passage or image and then clicking on the comment icon in the
-            floating menu.
-          </Text>
-        </HStack>
-      )}
-    </ScrollView>
+    <FlatList
+      ListHeaderComponent={Header}
+      ListEmptyComponent={EmptyState}
+      // grow-0 overrides default of ScrollView to keep the assigned width
+      style={tw`w-sidebar grow-0 bg-gray-100`}
+      ref={flatListRef}
+      data={comments}
+      renderItem={({ item }) => {
+        return <Comment comment={item} meId={me.id} meName={me.username} />;
+      }}
+      keyExtractor={(comment) => comment.id}
+    />
   );
 };
 
