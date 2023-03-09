a70fff56e93c072f33a6976144c7306319d04fea
Nik Graf <nik@nikgraf.com>
2023-03-09 06:23:29 +0100

    implement scrollto for comments in the sidebar (#696)
    * implement scrollTo for comments sidebar
    
    * switch to flatlist to make it work on ios
    
    * handle cases while loading

--- apps/app/components/commentsSidebar/CommentsSidebar.tsx
+++ apps/app/components/commentsSidebar/CommentsSidebar.tsx
@@ -1,81 +1,118 @@
-import React from "react";
 import {
   EditorBottombarDivider,
   EditorSidebarHeader,
   Icon,
-  ScrollView,
   Text,
   tw,
   View,
 } from "@serenity-tools/ui";
 import { useActor } from "@xstate/react";
 import { HStack } from "native-base";
-import { StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { FlatList, StyleSheet } from "react-native";
 import { usePage } from "../../context/PageContext";
 import { useMeQuery } from "../../generated/graphql";
 import Comment from "../comment/Comment";
 
-const CommentsSidebar: React.FC<{}> = () => {
+const styles = StyleSheet.create({
+  header: tw`justify-between`,
+  wrapper: tw`p-4 border-b border-gray-200`,
+});
+
+const Header: React.FC = () => {
+  return (
+    <EditorSidebarHeader style={styles.header}>
+      <Text variant="xs" bold>
+        Comments
+      </Text>
+      <HStack alignItems={"center"} style={tw`-mr-1`}>
+        <Text variant="xxs" muted style={tw`p-1`}>
+          Open
+        </Text>
+        <EditorBottombarDivider style={tw`h-4 border-r border-gray-600`} />
+        <Text variant="xxs" muted style={tw`p-1`}>
+          Resolved
+        </Text>
+      </HStack>
+    </EditorSidebarHeader>
+  );
+};
+
+const EmptyState: React.FC = () => {
+  return (
+    <HStack space={3} style={tw`p-4`}>
+      <View style={tw``}>
+        <Icon name="chat-1-line-message" color={"gray-500"} size={5} />
+      </View>
+      <Text variant="xs" muted>
+        Add suggestions, questions or appreciations by marking a text-passage or
+        image and then clicking on the comment icon in the floating menu.
+      </Text>
+    </HStack>
+  );
+};
+
+const CommentsSidebar: React.FC = () => {
   const { commentsService } = usePage();
   const [meResult] = useMeQuery();
   const [state] = useActor(commentsService);
+  const comments = state.context.decryptedComments;
+  const flatListRef = useRef<FlatList>(null);
+
+  commentsService.onTransition((state) => {
+    if (state.context.isOpenSidebar && flatListRef.current) {
+      if (
+        state.event.type === "HIGHLIGHT_COMMENT" &&
+        state.context.highlightedCommentId
+      ) {
+        const index = comments.findIndex(
+          (comment) => comment.id === state.context.highlightedCommentId
+        );
+        if (index === -1) return; // in case the list isn't yet loaded
+        flatListRef.current.scrollToIndex({
+          index,
+          animated: true,
+          viewPosition: 0,
+        });
+      } else if (
+        state.event.type === "OPEN_SIDEBAR" ||
+        state.event.type === "TOGGLE_SIDEBAR"
+      ) {
+        // should alays immediately scroll to top once the scrollbar opens
+        // and now highlighted comment is set
+        if (state.context.highlightedCommentId) {
+          const index = comments.findIndex(
+            (comment) => comment.id === state.context.highlightedCommentId
+          );
+          if (index === -1) return; // in case the list isn't yet loaded
+          flatListRef.current.scrollToIndex({
+            index,
+            animated: false,
+            viewPosition: 0,
+          });
+        } else {
+          flatListRef.current.scrollToOffset({ offset: 0, animated: false });
+        }
+      }
+    }
+  });
 
   const me = meResult.data?.me;
   if (!me) return null;
 
-  const comments = state.context.decryptedComments;
-
-  const styles = StyleSheet.create({
-    header: tw`justify-between`,
-    wrapper: tw`p-4 border-b border-gray-200`,
-  });
-
   return (
-    // grow-0 overrides default of ScrollView to keep the assigned width
-    <ScrollView style={tw`w-sidebar grow-0 bg-gray-100`}>
-      <EditorSidebarHeader style={styles.header}>
-        <Text variant="xs" bold>
-          Comments
-        </Text>
-        <HStack alignItems={"center"} style={tw`-mr-1`}>
-          <Text variant="xxs" muted style={tw`p-1`}>
-            Open
-          </Text>
-          <EditorBottombarDivider style={tw`h-4 border-r border-gray-600`} />
-          <Text variant="xxs" muted style={tw`p-1`}>
-            Resolved
-          </Text>
-        </HStack>
-      </EditorSidebarHeader>
-
-      {comments.length >= 1 ? (
-        <View>
-          {state.context.decryptedComments.map((comment) => {
-            if (!comment) return null;
-
-            return (
-              <Comment
-                key={`comment_${comment.id}`}
-                comment={comment}
-                meId={me.id}
-                meName={me.username}
-              />
-            );
-          })}
-        </View>
-      ) : (
-        <HStack space={3} style={tw`p-4`}>
-          <View style={tw``}>
-            <Icon name="chat-1-line-message" color={"gray-500"} size={5} />
-          </View>
-          <Text variant="xs" muted>
-            Add suggestions, questions or appreciations by marking a
-            text-passage or image and then clicking on the comment icon in the
-            floating menu.
-          </Text>
-        </HStack>
-      )}
-    </ScrollView>
+    <FlatList
+      ListHeaderComponent={Header}
+      ListEmptyComponent={EmptyState}
+      // grow-0 overrides default of ScrollView to keep the assigned width
+      style={tw`w-sidebar grow-0 bg-gray-100`}
+      ref={flatListRef}
+      data={comments}
+      renderItem={({ item }) => {
+        return <Comment comment={item} meId={me.id} meName={me.username} />;
+      }}
+      keyExtractor={(comment) => comment.id}
+    />
   );
 };
 
