bb54ff1adf1dbda250b891c816615be53993d03b
Nik Graf <nik@nikgraf.com>
2023-03-10 08:55:00 +0100

    fix editor comments on staging

--- packages/editor/tailwind.config.js
+++ packages/editor/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    "./**/*.{tsx,html}",
-    "../editor*/**/*.{html,tsx}", // also include other editor-packages like the editor-file-extension
+    "./**/*.{ts,tsx,html}",
+    "../editor*/**/*.{html,ts,tsx}", // also include other editor-packages like the editor-file-extension
   ],
   presets: [require("../../tailwind.config")],
   plugins: [require("@tailwindcss/typography")],
