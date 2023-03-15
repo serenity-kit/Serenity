72087fc75065fdee1c778d5a01b99b5e83b50bc2
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-03-15 14:05:09 +0100

    Adjust TextArea height depending on rows

--- packages/ui/components/textArea/TextArea.tsx
+++ packages/ui/components/textArea/TextArea.tsx
@@ -1,28 +1,47 @@
 import React, { useLayoutEffect, useRef } from "react";
 import { Platform } from "react-native";
-import { RawInput } from "../rawInput/RawInput";
+import { RawInput, RawInputProps } from "../rawInput/RawInput";
 
-const twoRowsHeight = 58;
+export type TextAreaProps = RawInputProps & {
+  minRows?: number;
+  maxRows?: number;
+};
 
-export const TextArea = (props) => {
+const calculateHeight = (rows: number) => {
+  // rows * lineheight + top-padding as the bottom padding needs to be on the container
+  return rows * 18 + 16;
+};
+
+export const TextArea = (props: TextAreaProps) => {
+  const { minRows = 2, maxRows = 5 } = props;
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const minRowsHeight = calculateHeight(minRows);
 
   useLayoutEffect(() => {
     if (Platform.OS === "web" && textareaRef.current) {
       // reset the height to get the correct scrollHeight for the textarea
       textareaRef.current.style.height = "0px";
       const height = textareaRef.current.scrollHeight;
-      textareaRef.current.style.height = `${Math.max(height, twoRowsHeight)}px`;
+      textareaRef.current.style.height = `${Math.max(height, minRowsHeight)}px`;
     }
-  }, [props.value]);
+  }, [props.value, minRowsHeight]);
 
   return (
     <RawInput
-      minHeight={twoRowsHeight}
+      // px needed as a sheer number can result in inconsistent behaviour, as nativebase has a type ISizes
+      // and if the number matches with one from this type it is automagically converted in a different
+      // px-size (entered number x 4) => e.g. 10 => 40px but 11 => 11px and then 12 => 48px
+      minHeight={`${minRowsHeight}px`}
       height="auto"
       {...props}
       multiline
       ref={textareaRef}
+      _input={{
+        // needs to be on the textarea element directly for correct overflow scroll behaviour
+        minHeight: `${minRowsHeight}px`,
+        maxHeight: `${calculateHeight(maxRows)}px`,
+      }}
     />
   );
 };
