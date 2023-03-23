9a357ac1d3878eadcb7d72b429a96330ef49daaf
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-03-23 17:14:21 +0100

    Refactor and improve styling to use custom properties

--- packages/editor/extensions/naishoAwarnessExtension/naishoAwarenessExtension.ts
+++ packages/editor/extensions/naishoAwarnessExtension/naishoAwarenessExtension.ts
@@ -23,15 +23,12 @@ export const AwarnessExtension = Extension.create<
       awareness: undefined,
       render: (user) => {
         const cursor = document.createElement("span");
-        console.log("user", user);
-
+        cursor.style.setProperty("--collab-color", user.color);
         cursor.classList.add("collaboration-cursor__caret");
-        cursor.setAttribute("style", `border-color: ${user.color}`);
 
         const label = document.createElement("div");
-
         label.classList.add("collaboration-cursor__label");
-        label.setAttribute("style", `background-color: ${user.color}`);
+
         label.insertBefore(document.createTextNode(user.name), null);
         cursor.insertBefore(label, null);
 
