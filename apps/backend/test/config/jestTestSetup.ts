3d96ac63c1c8158fc959731b72b4a3addf6ae14d
Nik Graf <nik@nikgraf.com>
2023-03-30 06:35:51 +0200

    debug

--- apps/backend/test/config/jestTestSetup.ts
+++ apps/backend/test/config/jestTestSetup.ts
@@ -1,4 +1,5 @@
 import sodium from "libsodium-wrappers";
+import sodium2 from "react-native-libsodium";
 
 // @ts-expect-error
 global.setImmediate = jest.useRealTimers;
@@ -7,4 +8,5 @@ jest.setTimeout(25000);
 
 beforeEach(async () => {
   await sodium.ready;
+  await sodium2.ready;
 });
