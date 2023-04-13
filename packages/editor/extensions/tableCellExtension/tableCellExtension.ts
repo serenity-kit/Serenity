e6b47924170bd932b51d00820ef2e09d2674ca80
Nik Graf <nik@nikgraf.com>
2023-04-13 13:54:20 +0200

    fix table insert structure

--- packages/editor/extensions/tableCellExtension/tableCellExtension.ts
+++ packages/editor/extensions/tableCellExtension/tableCellExtension.ts
@@ -1,5 +1,6 @@
 import TableCell from "@tiptap/extension-table-cell";
 
+// the default also allows to nest tables which we don't want
 export const tableCellContent =
   "paragraph+ | heading+ | codeBlock+ | blockquote+ | orderedList+ | bulletList+ | taskList+ | file+";
 
