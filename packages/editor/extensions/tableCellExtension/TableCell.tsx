7e125a7acdd6559c47124f1eda9e7c665c153735
Nik Graf <nik@nikgraf.com>
2023-04-13 16:52:16 +0200

    allow to add rows and hide row/column insert buttons via css

--- packages/editor/extensions/tableCellExtension/TableCell.tsx
+++ packages/editor/extensions/tableCellExtension/TableCell.tsx
@@ -1,34 +1,35 @@
 import { Icon } from "@serenity-tools/ui";
 import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
-import { findCell } from "prosemirror-tables";
+import { TableMap, addRow, findCell } from "prosemirror-tables";
 import React from "react";
 
 export const TableCell = (props: any) => {
-  console.log(props);
-
-  const resolvedPos = props.editor.view.state.doc.resolve(props.getPos());
-  const cellPositionInfo = findCell(resolvedPos);
+  // console.log(props);
 
   return (
     <NodeViewWrapper>
-      {/* only show when in first column */}
-      {cellPositionInfo?.left === 0 ? (
-        <div
-          className="insert-row"
-          onClick={() => {
-            console.log(cellPositionInfo);
-          }}
-        >
-          <Icon name="add-line" />
-        </div>
-      ) : null}
+      <div
+        className="insert-row"
+        onClick={() => {
+          const editor = props.editor.storage.tableCell.currentEditor;
+          const state = editor.view.state;
+          const resolvedPos = state.doc.resolve(props.getPos());
+          const cellPositionInfo = findCell(resolvedPos);
+          const table = resolvedPos.node(-1);
+          const tableStart = resolvedPos.start(-1);
+          const map = TableMap.get(table);
+          const tableRect = { ...cellPositionInfo, table, tableStart, map };
+          editor.view.dispatch(
+            addRow(state.tr, tableRect, cellPositionInfo.top)
+          );
+        }}
+      >
+        <Icon name="add-line" />
+      </div>
 
-      {/* only show when in first row */}
-      {cellPositionInfo?.top === 0 ? (
-        <div className="insert-column">
-          <Icon name="add-line" />
-        </div>
-      ) : null}
+      <div className="insert-column">
+        <Icon name="add-line" />
+      </div>
 
       <NodeViewContent className="content" />
     </NodeViewWrapper>
--- packages/editor/extensions/tableCellExtension/tableCellExtension.ts
+++ packages/editor/extensions/tableCellExtension/tableCellExtension.ts
@@ -9,6 +9,19 @@ export const tableCellContent =
 export const TableCellExtension = TiptapTableCellExtension.extend({
   content: tableCellContent,
 
+  addStorage() {
+    return {
+      // the ReactNodeViewRenderer doesn't re-render except it's content, attributes or selection changes
+      // and therefor we can have an outdated editor reference in the React component which can result
+      // in errors when we try to dispatch an action within the editor e.g. adding a row above an existing row
+      currentEditor: null,
+    };
+  },
+
+  onUpdate() {
+    this.storage.currentEditor = this.editor;
+  },
+
   addNodeView() {
     return ReactNodeViewRenderer(TableCell, {
       as: "td",
