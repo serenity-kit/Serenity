ee7713689687297bac99af5badf1661e7bfa00f3
Nik Graf <nik@nikgraf.com>
2023-04-13 15:28:27 +0200

    Only render inserts regarding the resolvedPosition of the cell

--- packages/editor/extensions/tableCellExtension/TableCell.tsx
+++ packages/editor/extensions/tableCellExtension/TableCell.tsx
@@ -1,18 +1,34 @@
 import { Icon } from "@serenity-tools/ui";
 import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
+import { findCell } from "prosemirror-tables";
 import React from "react";
 
 export const TableCell = (props: any) => {
-  // console.log(props.node);
+  console.log(props);
+
+  const resolvedPos = props.editor.view.state.doc.resolve(props.getPos());
+  const cellPositionInfo = findCell(resolvedPos);
 
   return (
     <NodeViewWrapper>
-      <div className="insert-row">
-        <Icon name="add-line" />
-      </div>
-      <div className="insert-column">
-        <Icon name="add-line" />
-      </div>
+      {/* only show when in first column */}
+      {cellPositionInfo?.left === 0 ? (
+        <div
+          className="insert-row"
+          onClick={() => {
+            console.log(cellPositionInfo);
+          }}
+        >
+          <Icon name="add-line" />
+        </div>
+      ) : null}
+
+      {/* only show when in first row */}
+      {cellPositionInfo?.top === 0 ? (
+        <div className="insert-column">
+          <Icon name="add-line" />
+        </div>
+      ) : null}
 
       <NodeViewContent className="content" />
     </NodeViewWrapper>
