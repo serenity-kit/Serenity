8341d4f22597f72f052622ccf527b8410e5e01a8
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-05-03 19:56:08 +0200

    Add add-row/column logic

--- packages/editor/extensions/tableExtension/Table.tsx
+++ packages/editor/extensions/tableExtension/Table.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@serenity-tools/ui";
+import { TableMap, addColumn, addRow } from "@tiptap/pm/tables";
 import { NodeViewContent, NodeViewWrapper } from "@tiptap/react";
 import React from "react";
 
@@ -6,6 +7,38 @@ export const Table = (props: any) => {
   const [active, setActive] = React.useState(false);
 
   props.editor.storage.table.setTableActive = setActive;
+
+  const getTableInsertInfo = () => {
+    const editor = props.editor.storage.tableCell.currentEditor;
+    const state = editor.view.state;
+    const resolvedPos = state.doc.resolve(props.getPos());
+
+    const table = props.node;
+    const tableStart = resolvedPos.start(1);
+    const map = TableMap.get(table);
+
+    const rowCount = map.height;
+    const colCount = map.width;
+
+    // info of last cell in table
+    const cellPositionInfo = {
+      left: colCount - 1,
+      top: rowCount - 1,
+      right: colCount,
+      bottom: rowCount,
+    };
+
+    // console.log("table: resPos ", resolvedPos);
+    // console.log("table: cellInfo ", cellPositionInfo);
+    // console.log("table: table ", table);
+    // console.log("table: tableStart ", tableStart);
+    // console.log("table: rows ", rowCount);
+    // console.log("table: columns ", colCount);
+    // console.log("map: ", map);
+
+    const tableRect = { ...cellPositionInfo, table, tableStart, map };
+    return { tr: state.tr, tableRect, cellPositionInfo };
+  };
 
   return (
     <NodeViewWrapper>
@@ -20,10 +53,26 @@ export const Table = (props: any) => {
       >
         <Icon name="arrow-down-filled" /> {active ? "Active" : "Not Active"}
       </div> */}
-      <div className="add add-column">
+      <div
+        className="add add-column"
+        onClick={() => {
+          const editor = props.editor.storage.tableCell.currentEditor;
+          const { tr, tableRect, cellPositionInfo } = getTableInsertInfo();
+          editor.view.dispatch(
+            addColumn(tr, tableRect, cellPositionInfo.right)
+          );
+        }}
+      >
         <Icon name="add-line" color="gray-600" />
       </div>
-      <div className="add add-row">
+      <div
+        className="add add-row"
+        onClick={() => {
+          const editor = props.editor.storage.tableCell.currentEditor;
+          const { tr, tableRect, cellPositionInfo } = getTableInsertInfo();
+          editor.view.dispatch(addRow(tr, tableRect, cellPositionInfo.bottom));
+        }}
+      >
         <Icon name="add-line" color="gray-600" />
       </div>
       <div className="row-line hidden"></div>
