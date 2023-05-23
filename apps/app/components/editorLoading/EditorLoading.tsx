eb405eb1f7a66383eaae3f6adb68aafbd31b5478
Susanne Kristufek <kristufek.susanne@gmail.com>
2023-05-23 19:06:57 +0200

    Adjust wrapper-styling for EditorLiading to mimic Editor default look

--- apps/app/components/editorLoading/EditorLoading.tsx
+++ apps/app/components/editorLoading/EditorLoading.tsx
@@ -3,6 +3,7 @@ import {
   CenterContent,
   Spinner,
   View,
+  tw,
   useHasEditorSidebar,
 } from "@serenity-tools/ui";
 
@@ -10,26 +11,30 @@ export const EditorLoading = () => {
   const hasEditorSidebar = useHasEditorSidebar();
 
   return (
-    <View>
+    <View style={tw`flex h-full flex-auto flex-row`}>
       <CenterContent>
         <Spinner fadeIn />
       </CenterContent>
       {/* need to show a mocked editor sidebar since it is loaded only inside the editor
       once all the data is available */}
       {hasEditorSidebar ? (
-        <EditorSidebar
-          // @ts-expect-error fake editor
-          editor={{ isActive: () => false }}
-          documentState="loading"
-          encryptAndUploadFile={() =>
-            Promise.resolve({
-              key: "",
-              nonce: "",
-              fileId: "",
-            })
-          }
-          headingLevels={[1, 2, 3]}
-        />
+        <View
+          style={tw`w-sidebar grow-0 shrink-0 border-l border-gray-200 bg-gray-100`}
+        >
+          <EditorSidebar
+            // @ts-expect-error fake editor
+            editor={{ isActive: () => false }}
+            documentState="loading"
+            encryptAndUploadFile={() =>
+              Promise.resolve({
+                key: "",
+                nonce: "",
+                fileId: "",
+              })
+            }
+            headingLevels={[1, 2, 3]}
+          />
+        </View>
       ) : null}
     </View>
   );
