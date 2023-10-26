6bb51dad3293479ac2407b8f539d388cbd2cda77
Nik Graf <nik@nikgraf.com>
2023-10-26 18:07:20 +0200

    stop retries on the e2e test suite

--- apps/backend/playwright.config.ts
+++ apps/backend/playwright.config.ts
@@ -7,7 +7,8 @@ dotenv.config({ path: path.resolve(__dirname, ".e2e-tests.env") });
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   globalSetup: require.resolve("./test/config/playwrightGlobalSetup"),
-  retries: process.env.CI ? 1 : 0,
+  // retries: process.env.CI ? 1 : 0,
+  retries: 0, // retries make no sense since e2e tests often rely on previous run tests (only in the same file)
   workers: process.env.CI ? 1 : 1,
   use: { trace: "on-first-retry" },
   projects: [
