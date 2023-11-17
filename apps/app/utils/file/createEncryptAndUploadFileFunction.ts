3c19d64a383f3c4f8ffb65ddff56140eeadb42ec
Nik Graf <nik@nikgraf.com>
2023-11-17 09:15:10 +0100

    upgrade upload packages

--- apps/app/utils/file/createEncryptAndUploadFileFunction.ts
+++ apps/app/utils/file/createEncryptAndUploadFileFunction.ts
@@ -36,6 +36,7 @@ export const createEncryptAndUploadFileFunction = ({
     const response = await fetch(uploadUrl, {
       method: "PUT",
       body: fileCiphertext,
+      // mode: "cors",
     });
     if (!response || response.status !== 200) {
       throw new Error("Failed to upload the file");
