b446fc518c3b933714d2f45849f0d10c4a1169bc
Nik Graf <nik@nikgraf.com>
2023-11-17 21:43:59 +0100

    encrypt the DB with a key stored in the secure store

--- apps/app/store/sql/sql.ts
+++ apps/app/store/sql/sql.ts
@@ -1,14 +1,28 @@
 import { OPSQLiteConnection, open } from "@op-engineering/op-sqlcipher";
+import * as SecureStore from "expo-secure-store";
+import sodium from "libsodium-wrappers";
+
+export const serenityDbKeyId = "serenity-db.key";
 
 let db: OPSQLiteConnection;
 
-export const ready = () => {
+export const ready = async () => {
   if (db) {
-    return Promise.resolve();
+    return undefined;
   }
+
+  await sodium.ready;
+  let key = await SecureStore.getItemAsync(serenityDbKeyId);
+  if (!key) {
+    key = sodium.to_base64(
+      sodium.randombytes_buf(sodium.crypto_secretbox_KEYBYTES)
+    );
+    await SecureStore.setItemAsync(serenityDbKeyId, key);
+  }
+
   db = open({
     name: "serenity-db",
-    encryptionKey: "SDASDWQEWQWQEQWESXVEFWEADASDFSAD",
+    encryptionKey: key,
     inMemory: true,
   });
   return Promise.resolve();
@@ -19,7 +33,10 @@ export const resetInMemoryDatabase = () => {};
 export const triggerDebouncedDatabasePersisting = () => {};
 
 export const destroyPersistedDatabase = async () => {
-  return Promise.resolve();
+  db.delete();
+  await SecureStore.deleteItemAsync(serenityDbKeyId);
+  // creates a new empty DB that is encrypted with a new key
+  await ready();
 };
 
 export const execute = (
